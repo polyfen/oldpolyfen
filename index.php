7fd5326b58cbc2b9112237cd88f0b7b85b252aba
Boris Hrnčić <services@borishrncic.com>
2018-07-13 18:01:31 +0300

    Added basic HTML body content

--- index.php
+++ index.php
@@ -11,13 +11,29 @@
       <?php include 'sections/header.php';?>
     <!-- INCLUDE HEADER.PHP end -->
 
-    <div class="container-fluid">
+    <section id="cover">
+        <h1>A creative agency devoted to brand identity design.</h1>
+    </section>
+
+    <section class="container-fluid" id="about">
       <div class="row">
-
-
-
+        <div class="col">
+          <h2>We mean business</h2>
+          <p>Our small and efficient team of experts from around the world is run by four partners with backgrounds in business, design, tech, and marketing.</p>
+          <p>Our services are a solution for executives working at new and emerging businesses who need professional guidance in translating their business into a creative brand identity that will stand-out over the competition and increase revenue.</p>
+        </div>
       </div>
-    </div>
+    </section>
+
+    <section class="container-fluid" id="about">
+      <div class="row">
+        <div class="col">
+          <h2>Give your business the brand it deserves</h2>
+          <p>Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.</p>
+          <button>Contact us</button>
+        </div>
+      </div>
+    </section>
 
     <!-- INCLUDE FOOTER.PHP start -->
       <?php include 'sections/footer.php';?>
