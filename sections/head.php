a2a22fc6e18706f3ad82c6f65d906518a304cb86
Boris Hrnčić <services@borishrncic.com>
2018-07-14 14:20:50 +0300

    Added font files

--- sections/head.php
+++ sections/head.php
@@ -43,7 +43,7 @@
   <!-- CSS Page-Specific-->
 
   <!-- FONTS -->
-  <!-- [TBD] fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
 
 
