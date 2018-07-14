b8f0515eb08a6e3909f513423e052a1ee50cf943
Boris Hrnčić <services@borishrncic.com>
2018-07-14 19:42:08 +0300

    Added footer html content

--- sections/footer.php
+++ sections/footer.php
@@ -1,5 +1,18 @@
 <footer>
 
-  <!-- [TBD] footer -->
+<div id="social-icons">
+  <ul>
+      <li><a href="http://polyfen.com/facebook" target="_blank" title="Facebook"><i class="fab fa-facebook-square"></i></a></li>
+      <li><a href="http://polyfen.com/twitter" target="_blank" title="Twitter"><i class="fab fa-twitter"></i></a></li>
+      <li><a href="http://polyfen.com/instagram" target="_blank" title="Instagram"><i class="fab fa-instagram"></i></a></li>
+      <li><a href="http://polyfen.com/dribbble" target="_blank" title="Dribbble"><i class="fab fa-dribbble"></i></a></li>
+      <li><a href="http://polyfen.com/behance" target="_blank" title="Behance"><i class="fab fa-behance"></i></a></li>
+      <li><a href="http://polyfen.com/linkedin" target="_blank" title="Linkedin"><i class="fab fa-linkedin-in"></i></a></li>
+  </ul>
+</div>
+
+<div id="legal">
+  <a><small>Privacy Policy</small></a>
+</div>
 
 </footer>
