5778bdc77075cd95935a4ff6624222b06ad8b662
Boris Hrnčić <services@borishrncic.com>
2018-07-20 12:16:15 +0300

    Added Google Analytics tracking code

--- sections/head.php
+++ sections/head.php
@@ -41,8 +41,6 @@
   <link rel="stylesheet" href="libs/css/sections.css" type="text/css">
   <link rel="stylesheet" href="libs/css/modal.css" type="text/css">
 
-  <!-- CSS Page-Specific-->
-
   <!-- FONTS -->
   <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
@@ -72,6 +70,17 @@
   */
   -->
   <link rel="stylesheet" type="text/css" href="libs/fonts/MyFontsWebfontsKit.css">
+
+  <!-- Analytics -->
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-122656172-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-122656172-1');
+  </script>
 
 
 </head>
