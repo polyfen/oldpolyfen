91d100e17f2d5f608c97fab57bbf989e36891f26
Boris Hrncic <services@borishrncic.com>
2018-09-09 19:07:24 -0300

    General update
    Increased size of H1
    Made H2 bold
    Updated colour scheme
    Updated cover image
    Updated Contact Form styles

--- index.php
+++ index.php
@@ -32,14 +32,17 @@
         <div class="col" id="cta-content">
           <h2>Give your business the brand it deserves</h2>
           <p>Our services are a solution for emerging businesses who need guidance in translating their business into a creative brand identity.</p>
-          <a class="button primary-button" onclick="document.getElementById('contact-form').className='col c-block';document.getElementById('cta-content').className='col d-none';">Contact us</a>
+          <a class="button primary-button" onclick="document.getElementById('contact-form').className='col c-block';document.getElementById('cta-content').className='col d-none';document.getElementById('name-input').focus(); return false;">Contact us</a>
         </div>
 
         <div class="col d-none" id="contact-form">
+          <h2>Contact us</h2>
           <form id="ajax-contact" method="post" action="libs/contact-form/mailer.php">
-            <input type="text" name="name" placeholder="Your name" required>
+            <input type="text" name="name" placeholder="Your name" id="name-input" required>
             <input type="email" name="email" id="email" placeholder="Your email" required>
             <textarea name="message" id="message" placeholder="Your message..." required></textarea>
+            <div id="captcha-wrapper">
+            </div>
             <button type="submit" id="send">Send</button>
             <button id="form-loader" class="d-none">
               <i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
