d395580797b3ca3cfaa8838f0d3a058b5a2ecfe8
Boris Hrncic <services@borishrncic.com>
2018-11-23 18:58:33 -0300

    Deleted useless rotation javascript function

--- sections/head.php
+++ sections/head.php
@@ -79,13 +79,6 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
 
   <!-- PAGE-SPECIFIC SCRIPTS -->
-    <script type="text/javascript">
-
-        $('.rotate').onLoad(function() {
-        $(this).toggleClass('rotated');
-    });
-
-    </script>
 
     <script type="text/javascript">
 
