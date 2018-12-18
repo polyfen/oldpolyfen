2d4197632c51983303f24a59f716322d926d9118
Boris Hrncic <services@borishrncic.com>
2018-12-18 15:59:24 +0100

    Added LG styles

--- index.php
+++ index.php
@@ -38,12 +38,12 @@
 
             <section class="case-project">
               <div id="polyfen-cover" class="project-cover polyfen-cover-INACTIVE"></div>
-              <div id="polyfen-thumbnail" class="project-thumbnail polyfen-thumbnail-INACTIVE d-none d-sm-none d-md-none d-lg-block d-xl-block">
+              <div id="polyfen-thumbnail" class="project-thumbnail polyfen-thumbnail-INACTIVE d-none d-sm-none d-md-none d-lg-none d-xl-block">
                 <img src="imgs/case-studies/developing-our-own-brand/polyfen-ideogram-grid.gif" height="100%" width="auto" style="border-radius:8px;">
               </div>
               <div class="project-description">
                 <h3>Developing our own brand</h3>
-                <p><strong>Polyfen</strong> stands for one core concept, the midpoint between business and design. <br class="d-none d-sm-none d-md-none d-lg-inline d-xl-inline"/><span class="d-none d-sm-none d-md-inline d-lg-inline d-xl-inline">A clean and professional look with a distinctive and creative flair.</span></p>
+                <p><strong>Polyfen</strong> stands for one core concept, the midpoint between business and design. <br class="d-none d-sm-none d-md-none d-lg-none d-xl-inline"/><span class="d-none d-sm-none d-md-inline d-lg-inline d-xl-inline">A clean and professional look with a distinctive and creative flair.</span></p>
                 <a class="secondary-button">View project ›</a>
               </div>
             </section>
