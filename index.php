74489f23a363d3dcdbb69d17e5a21e08b6093a5d
Boris Hrncic <services@borishrncic.com>
2018-12-19 17:18:17 +0100

    Completed XL and XXL styles

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
               </div>
               <div class="project-description">
                 <h3>Developing our own brand</h3>
-                <p><strong>Polyfen</strong> stands for one core concept, the midpoint between business and design. <span class="d-none d-sm-none d-md-inline d-lg-inline d-xl-inline">A clean and professional look with a distinctive and creative flair.</span></p>
+                <p><strong>Polyfen</strong> stands for one core concept, the midpoint between business and design. <span class="d-none d-sm-none d-md-inline d-lg-inline d-xl-inline">A clean and professional look with a creative flair.</span></p>
                 <a class="secondary-button">View project ›</a>
               </div>
             </section>
