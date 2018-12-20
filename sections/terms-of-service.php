ef981f86b15a610e41b8ff6a1ca67f84ff98a5a3
Boris Hrncic <services@borishrncic.com>
2018-12-20 16:15:01 +0100

    Completed XS and SM styles for modal window

--- sections/terms-of-service.php
+++ sections/terms-of-service.php
@@ -5,9 +5,10 @@
       <h3>Terms of Service</h3>
     </div>
     <div class="modal-window-content">
-      <h3>1. Terms</h3>
+      <h4>1. Terms</h4>
       <p>By accessing the website at <a href="http://polyfen.com">http://polyfen.com</a>, you are agreeing to be bound by these terms of service, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this website are protected by applicable copyright and trademark law.</p>
-      <h3>2. Use License</h3>
+      <hr>
+      <h4>2. Use License</h4>
       <ol type="a">
          <li>Permission is granted to temporarily download one copy of the materials (information or software) on Polyfen's website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:
          <ol type="i">
@@ -20,20 +21,26 @@
           </li>
          <li>This license shall automatically terminate if you violate any of these restrictions and may be terminated by Polyfen at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.</li>
       </ol>
-      <h3>3. Disclaimer</h3>
+      <hr>
+      <h4>3. Disclaimer</h4>
       <ol type="a">
          <li>The materials on Polyfen's website are provided on an 'as is' basis. Polyfen makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties including, without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights.</li>
          <li>Further, Polyfen does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its website or otherwise relating to such materials or on any sites linked to this site.</li>
       </ol>
-      <h3>4. Limitations</h3>
+      <hr>
+      <h4>4. Limitations</h4>
       <p>In no event shall Polyfen or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the materials on Polyfen's website, even if Polyfen or a Polyfen authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.</p>
-      <h3>5. Accuracy of materials</h3>
+      <hr>
+      <h4>5. Accuracy of materials</h4>
       <p>The materials appearing on Polyfen's website could include technical, typographical, or photographic errors. Polyfen does not warrant that any of the materials on its website are accurate, complete or current. Polyfen may make changes to the materials contained on its website at any time without notice. However Polyfen does not make any commitment to update the materials.</p>
-      <h3>6. Links</h3>
+      <hr>
+      <h4>6. Links</h4>
       <p>Polyfen has not reviewed all of the sites linked to its website and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by Polyfen of the site. Use of any such linked website is at the user's own risk.</p>
-      <h3>7. Modifications</h3>
+      <hr>
+      <h4>7. Modifications</h4>
       <p>Polyfen may revise these terms of service for its website at any time without notice. By using this website you are agreeing to be bound by the then current version of these terms of service.</p>
-      <h3>8. Governing Law</h3>
+      <hr>
+      <h4>8. Governing Law</h4>
       <p>These terms and conditions are governed by and construed in accordance with the laws of Estonia and you irrevocably submit to the exclusive jurisdiction of the courts in that State or location.</p>
     </div>
   </div>
