65f232342ab900ef896c682b48452d3ccd6b5858
Boris Hrncic <services@borishrncic.com>
2018-12-25 11:40:49 +0100

    Separated CTA into a section/.php file

--- developing-our-brand.php
+++ developing-our-brand.php
@@ -19,8 +19,27 @@
   <meta name="robots" content="index, follow">
 
   <!-- PAGE-SPECIFIC CSS -->
-  <link rel="stylesheet" href="app/css/developing-our-brand.css" type="text/css">
+  <link rel="stylesheet" href="app/css/case-studies.css" type="text/css">
 
+  <!-- PAGE-SPECIFIC SCRIPTS -->
+  <script type="text/javascript">
+  $(window).scroll(function () {
+    var st = $(window).scrollTop();
+    var scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
+    if (scrollBottom <= 200) {
+          $("#pearl-07").removeClass("pearl-07-INACTIVE").addClass("pearl-07-ACTIVE");
+          $("#pearl-08").removeClass("pearl-08-INACTIVE").addClass("pearl-08-ACTIVE");
+          $("#pearl-09").removeClass("pearl-09-INACTIVE").addClass("pearl-09-ACTIVE");
+          $("#pearl-10").removeClass("pearl-10-INACTIVE").addClass("pearl-10-ACTIVE");
+      }
+    if (scrollBottom > 200) {
+          $("#pearl-07").addClass("pearl-07-INACTIVE").removeClass("pearl-07-ACTIVE");
+          $("#pearl-08").addClass("pearl-08-INACTIVE").removeClass("pearl-08-ACTIVE");
+          $("#pearl-09").addClass("pearl-09-INACTIVE").removeClass("pearl-09-ACTIVE");
+          $("#pearl-10").addClass("pearl-10-INACTIVE").removeClass("pearl-10-ACTIVE");
+      }
+    });
+  </script>
   </head>
 
   <body id="developing-our-brand" class="case-study">
@@ -32,6 +51,10 @@
     <section>
       <h1>Developing our own brand</h1>
     </section>
+
+    <!-- INCLUDE FOOTER.PHP start -->
+      <?php include 'sections/call-to-action.php';?>
+    <!-- INCLUDE FOOTER.PHP end -->
 
     <!-- INCLUDE FOOTER.PHP start -->
       <?php include 'sections/footer.php';?>
--- index.php
+++ index.php
@@ -303,45 +303,9 @@
 
     </div>
 
-    <section id="call-to-action">
-
-      <div id="cta-content">
-        <h2>Give your business the brand it deserves</h2>
-        <a class="button primary-button" onclick="document.getElementById('contact-form').className='col c-block';document.getElementById('call-to-action').className='active-call-to-action';document.getElementById('cta-content').className='col d-none';document.getElementById('name-input').focus(); return false;">Contact us</a>
-      </div>
-
-      <div class="d-none" id="contact-form">
-        <h2>Contact us</h2>
-        <form id="ajax-contact" method="post" action="app/contact-form/mailer.php">
-          <input type="text" name="name" placeholder="Your name" id="name-input" required>
-          <input type="email" name="email" id="email" placeholder="Your email" required>
-          <textarea name="message" id="message" placeholder="Your message..." required></textarea>
-          <button type="submit" id="send">Send</button>
-          <button id="form-loader" class="d-none">
-            <i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
-            <span class="sr-only">Loading...</span>
-          </button>
-        </form>
-        <button id="go-back" onclick="document.getElementById('cta-content').className='col c-block';document.getElementById('contact-form').className='col d-none';document.getElementById('call-to-action').className='';">
-          <i class="fa fa-chevron-left"></i> Back
-        </button>
-      </div>
-
-      <div class="curve">
-      </div>
-
-      <div class="background">
-        <div id="pearl-10" class="pearl pearl-10-INACTIVE rotate">
-        </div>
-        <div id="pearl-09" class="pearl pearl-09-INACTIVE rotate">
-        </div>
-        <div id="pearl-08" class="pearl pearl-08-INACTIVE rotate-slower">
-        </div>
-        <div id="pearl-07" class="pearl pearl-07-INACTIVE rotate">
-        </div>
-      </div>
-
-    </section>
+    <!-- INCLUDE FOOTER.PHP start -->
+      <?php include 'sections/call-to-action.php';?>
+    <!-- INCLUDE FOOTER.PHP end -->
 
     <!-- INCLUDE FOOTER.PHP start -->
       <?php include 'sections/footer.php';?>
