7b0d8f891d93573a0fb7884342f777395a81b17d
Boris Hrncic <services@borishrncic.com>
2018-12-25 21:09:45 +0100

    Updated title section styles

--- developing-our-brand.php
+++ developing-our-brand.php
@@ -57,24 +57,28 @@
     </section>
 
     <section id="brand-research">
-      <h2>Brand Research</h2>
+      <h2>1. Brand Research</h2>
       <p>We conducted an exhaustive analysis of our business proposal, our industry and competitors, and our audience.</p>
-      <p>Our conclusion was that most of our competitors market themselves as (A) business-oriented targeting an executive audience with a very sober and simple visual identity, while (B) the other half of our competitors focus on expressing their creative talent gaining popularity amongst the creative industry with a very quirky and playful brand.</p>
+      <p>Our conclusion was that most of our competitors market themselves as</p>
+      <ol>
+        <li>business-oriented targeting an executive audience with a very sober and simple visual identity, while</li>
+        <li>the other half of our competitors focus on expressing their creative talent gaining popularity amongst the creative industry with a very quirky and playful brand.</li>
+      </ol>
       <p>Based on our business proposal, our conclusion was to position our brand as something in between the business-oriented and the peculiarly creative.</p>
     </section>
 
     <section id="brand-research">
-      <h2>Brand Strategy</h2>
+      <h2>2. Brand Strategy</h2>
       <p>We developed a strategy for expressing our business proposal as a brand focused on the core concept <strong>The midpoint between business & creativity</strong>.</p>
       <p>We adopted <strong>Polyfen</strong> as a short, simple, memorable brand name that also carries a sound of playfulness as an abstract word with no other meaning.</p>
-      <img src="imgs/case-studies/developing-our-own-brand/MOODBOARD_PLY_Brand.png" style="max-width:100%;border-radius:8px;">
+      <img src="imgs/case-studies/developing-our-own-brand/MOODBOARD_PLY_Brand.png" id="moodboard">
     </section>
 
     <div style="clear:both;">
     </div>
 
     <section id="visual-identity">
-      <h2>Visual Identity</h2>
+      <h2>3. Visual Identity</h2>
       <p>Our core concept is reflected in visual form with a logo that is as distinctive and playful as possible while remaining as clean, simple, and memorable as possible. The same can be said of our other visual identity elements…</p>
       <p>Our typographic selection features <a href="https://www.myfonts.com/fonts/kastelov/intelo/" target="_blank">Intelo Alt</a> on headings, a clean and geometric typeface with a distinctively playful character, most noticeable in the roundness of the letter “e”; paired with <a href="https://www.myfonts.com/fonts/garagefonts/freight-text-pro/" target="_blank">Freight Text Pro</a>,  a clean and readable face for all body text.</p>
       <p>The colour scheme is characterised by a particular accent colour that can be identified as Polyfen Indigo, a vibrant and memorable hue between blue and purple.</p>
@@ -82,17 +86,17 @@
     </section>
 
     <section id="brand-guidelines">
-      <h2>Brand Guidelines</h2>
+      <h2>4. Brand Guidelines</h2>
       <p>To ensure a consistent use of all the parameters that we have established to identify our brand, we developed a web page with all guidelines and resources necessary for our organization. The web guidelines can be updated as our brand evolves, and it’s conveniently accessible at <a href="http://polyfen.com/brand" target="_blank">polyfen.com/brand</a>.</p>
     </section>
 
     <section id="brand-touchpoints">
-      <h2>Brand Touchpoints</h2>
+      <h2>5. Brand Touchpoints</h2>
       <p>We designed a system of all the branded applications that our business needed; Including email signatures, social media profiles, business cards, letterheads, invoices, and Presentation slides.</p>
     </section>
 
     <section id="web-development">
-      <h2>Web Development</h2>
+      <h2>6. Web Development</h2>
       <p>Our website, the centrepiece of our online presence, features concise and relevant content set in a clean and readable layout with prominent visual elements along with interactive animations which offer a unique user experience.</p>
     </section>
 
