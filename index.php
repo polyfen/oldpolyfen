ed9ebf7e16c35c2db561ab6d97f9c1022175e113
Boris Hrncic <services@borishrncic.com>
2018-12-27 17:32:22 +0100

    Worked on meta tags

--- index.php
+++ index.php
@@ -15,13 +15,13 @@
   <meta name="keywords" content="Brand Identity Design, Brand ID, Branding, Brand, Graphic, Design, Logo" />
   <meta name="description" property="og:description" content="Polyfen is a
   creative agency specializing in brand identity design. Our small and efficient team of experts from around the world is run by four partners with backgrounds in business, design, tech, and marketing." />
-  <meta property="og:image" content="thumbnail.png"/>
-  <meta name="twitter:card" content="summary" />
+  <meta property="og:image:url" content="http://polyfen.com/dev/web-5/imgs/meta/polyfen-thumbnail.png"/>
+  <meta name="twitter:card" content="summary_large_image" />
   <meta name="twitter:site" content="@polyfenstudio" />
   <meta name="twitter:title" content="Polyfen" />
   <meta name="twitter:description" content="Polyfen is a
   creative agency specializing in brand identity design. Our small and efficient team of experts from around the world is run by four partners with backgrounds in business, design, tech, and marketing." />
-  <meta name="twitter:image" content="thumbnail.png" />
+  <meta name="twitter:image" content="http://polyfen.com/dev/web-5/imgs/meta/polyfen-thumbnail.png" />
   <meta name="robots" content="index, follow">
 
   <!-- PAGE-SPECIFIC CSS -->
