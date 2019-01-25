b9365c159fdc12eea55d224c8848136517a70de0
Boris Hrncic <services@borishrncic.com>
2019-01-25 10:38:16 +0100

    Added ogg fallback file for web-cover video

--- case-study.php
+++ case-study.php
@@ -372,6 +372,7 @@
                 <div class="item active">
                   <video width="100%" height="auto" style="border-radius:8px;" autoplay loop>
                     <source src="imgs/case-studies/developing-our-own-brand/web-development/web-cover.mp4" type="video/mp4">
+                    <source src="imgs/case-studies/developing-our-own-brand/web-development/web-cover.ogv" type="video/ogg">
                     Your browser does not support the video tag.
                   </video>
                 </div>
