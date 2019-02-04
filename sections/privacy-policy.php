dc3d3907d8436c43a3041ded50cca14971d5aa11
Boris Hrncic <services@borishrncic.com>
2019-02-04 13:01:01 +0100

    Changed HTTP to HTTPS on legal modals.

--- sections/privacy-policy.php
+++ sections/privacy-policy.php
@@ -5,7 +5,7 @@
       <h3>Privacy Policy</h3>
     </div>
     <div class="modal-window-content">
-      <p>Your privacy is important to us. It is Polyfen's policy to respect your privacy regarding any information we may collect from you across our website, <a href="http://polyfen.com">http://polyfen.com</a>, and other sites we own and operate.</p>
+      <p>Your privacy is important to us. It is Polyfen's policy to respect your privacy regarding any information we may collect from you across our website, <a href="https://polyfen.com">https://polyfen.com</a>, and other sites we own and operate.</p>
       <p>We only ask for personal information when we truly need it to provide a service to you. We collect it by fair and lawful means, with your knowledge and consent. We also let you know why we’re collecting it and how it will be used.</p>
       <p>We only retain collected information for as long as necessary to provide you with your requested service. What data we store, we’ll protect within commercially acceptable means to prevent loss and theft, as well as unauthorised access, disclosure, copying, use or modification.</p>
       <p>We don’t share any personally identifying information publicly or with third-parties, except when required to by law.</p>
--- sections/terms-of-service.php
+++ sections/terms-of-service.php
@@ -6,7 +6,7 @@
     </div>
     <div class="modal-window-content">
       <h4>1. Terms</h4>
-      <p>By accessing the website at <a href="http://polyfen.com">http://polyfen.com</a>, you are agreeing to be bound by these terms of service, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this website are protected by applicable copyright and trademark law.</p>
+      <p>By accessing the website at <a href="https://polyfen.com">https://polyfen.com</a>, you are agreeing to be bound by these terms of service, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this website are protected by applicable copyright and trademark law.</p>
       <hr>
       <h4>2. Use License</h4>
       <ol type="a">
