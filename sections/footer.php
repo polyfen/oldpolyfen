3038619fa56b26cb3035d127d682bfd804386e5f
Boris Hrncic <services@borishrncic.com>
2019-06-25 21:09:22 -0300

    Desktop footer update

--- sections/footer.php
+++ sections/footer.php
@@ -10,12 +10,12 @@
 
 <div id="social-icons">
   <ul>
-      <li><a href="http://polyfen.com/facebook" target="_blank" title="Facebook"><i class="fab fa-facebook-square"></i></a></li>
-      <li><a href="http://polyfen.com/twitter" target="_blank" title="Twitter"><i class="fab fa-twitter"></i></a></li>
-      <li><a href="http://polyfen.com/instagram" target="_blank" title="Instagram"><i class="fab fa-instagram"></i></a></li>
-      <li><a href="http://polyfen.com/dribbble" target="_blank" title="Dribbble"><i class="fab fa-dribbble"></i></a></li>
-      <li><a href="http://polyfen.com/behance" target="_blank" title="Behance"><i class="fab fa-behance"></i></a></li>
-      <li><a href="http://polyfen.com/linkedin" target="_blank" title="Linkedin"><i class="fab fa-linkedin-in"></i></a></li>
+    <a href="http://polyfen.com/facebook" target="_blank" title="Facebook"><li><i class="fab fa-facebook-f"></i></li></a>
+    <a href="http://polyfen.com/twitter" target="_blank" title="Twitter"><li><i class="fab fa-twitter"></i></li></a>
+    <a href="http://polyfen.com/instagram" target="_blank" title="Instagram"><li><i class="fab fa-instagram"></i></li></a>
+    <a href="http://polyfen.com/dribbble" target="_blank" title="Dribbble"><li><i class="fab fa-dribbble"></i></li></a>
+    <a href="http://polyfen.com/behance" target="_blank" title="Behance"><li><i class="fab fa-behance"></i></li></a>
+    <a href="http://polyfen.com/linkedin" target="_blank" title="Linkedin"><li><i class="fab fa-linkedin-in"></i></li></a>
   </ul>
 </div>
 
