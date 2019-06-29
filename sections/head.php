37913cc99a86a6ee87beb4f634c17b907ae5cfe1
Boris Hrncic <services@borishrncic.com>
2019-06-29 14:08:48 -0300

    Removed request for embedding a font family which wasn't being used

--- sections/head.php
+++ sections/head.php
@@ -85,7 +85,6 @@
 -->
 <link rel="stylesheet" type="text/css" href="libs/fonts/MyFontsWebfontsKit.css">
 <link rel="stylesheet" href="https://use.typekit.net/aba8ecz.css">
-<link href="https://fonts.googleapis.com/css?family=Source+Serif+Pro&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
   <!-- Analytics -->
