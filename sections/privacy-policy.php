ae38b0563f7d7eb99f6b67bd1e330b6a71e198f3
Boris Hrncic <services@borishrncic.com>
2019-10-29 19:50:25 -0300

    GDPR compliance on Privacy Policy

--- sections/privacy-policy.php
+++ sections/privacy-policy.php
@@ -8,6 +8,9 @@
       <h4>Your privacy is important to us.</h4>
       <p>It is Polyfen's policy to respect your privacy regarding any information we may collect from you across our website, <a href="https://polyfen.com">https://polyfen.com</a>, and other sites we own and operate.</p>
       <p>We only ask for personal information when we truly need it to provide a service to you. We collect it by fair and lawful means, with your knowledge and consent. We also let you know why we’re collecting it and how it will be used.</p>
+      <p>We rely on simple web trackers, namely Google Analytics, in order to measure our web usage and provide a better browsing experience. Google Analytics may collect data such us your IP address, web browser, operative system, geographical region, usage activity, as well as other demographic information.</p>
+      <p>Other third-party tools on our website, namely Cloudfare, Pinterest, FontAwesome, and Typekit additionally rely on cookies to provide web features for the end-user.</p>
+      <p>Users submitting messages through our website’s contact form are willingly providing their name, email address, and might choose to provide further information in the body of the message.</p>
       <p>We only retain collected information for as long as necessary to provide you with your requested service. What data we store, we’ll protect within commercially acceptable means to prevent loss and theft, as well as unauthorised access, disclosure, copying, use or modification.</p>
       <p>We don’t share any personally identifying information publicly or with third-parties, except when required to by law.</p>
       <p>Our website may link to external sites that are not operated by us. Please be aware that we have no control over the content and practices of these sites, and cannot accept responsibility or liability for their respective privacy policies.</p>
