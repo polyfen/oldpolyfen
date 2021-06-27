3f30296da1b7da05d59f4b621e89f0638cfae57f
Boris Hrncic <services@borishrncic.com>
2021-06-27 16:48:41 -0300

    Removed mailto:

--- sections/call-to-action.php
+++ sections/call-to-action.php
@@ -3,7 +3,7 @@
 
   <div id="cta-content">
     <h2>Give your business the brand it deserves</h2>
-    <a class="button primary-button" href="mailto:hi@polyfen.com">Let's Talk!</a>
+    <h2>hi<span style="opacity:0.7;">[at]</span>polyfen.com</h2>
   </div>
 
 
