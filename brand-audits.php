1bae450431d841f593c2d83b34011f0552e0e744
Boris Hrnčić <borishrncic@users.noreply.github.com>
2022-10-09 14:13:28 +0200

    Brand Audits Initial Commit

---
+++ brand-audits.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <title>Polyfen | Get a free Brand Audit</title>
+
+
+    <!-- INCLUDE HEAD.PHP start -->
+    <?php include 'sections/head.php';?>
+    <!-- INCLUDE HEAD.PHP end -->
+
+    <!-- PAGE-SPECIFIC META start -->
+    <meta property="og:title" content="Get a free Brand Audit for your Business"/>
+    <meta property="og:url" content="https://polyfen.com/brand-audits"/>
+    <meta name="keywords" content="Brand, Brand Identity Design, Branding, Agency, Graphic Design, Logo, Creative, Strategy, Consultancy, Business Growth" />
+    <meta name="description" property="og:description" content="Apply for a full custom report listing how your business can improve its brand and website." />
+    <meta property="og:image" content="https://polyfen.com/imgs/meta/brand-audits.png"/>
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta property="fb:app_id" content="1799272553516195" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@PolyfenAgency" />
+    <meta name="twitter:title" content="Get a free Brand Audit for your Business" />
+    <meta name="twitter:description" content="Apply for a full custom report listing how your business can improve its brand and website." />
+    <meta name="twitter:image" content="https://polyfen.com/imgs/meta/brand-audits.png" />
+    <meta name="robots" content="index, follow">
+    <!-- PAGE-SPECIFIC META end -->
+
+    <!-- PAGE-SPECIFIC CSS start -->
+    <!-- PAGE-SPECIFIC CSS end -->
+
+    <!-- PAGE-SPECIFIC SCRIPTS start -->
+    <!-- PAGE-SPECIFIC SCRIPTS end -->
+
+
+  </head>
+
+  <body>
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P25JFWC"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <!-- FORCE MAX-WIDTH 100% start -->
+    <div class="main-wrapper">
+
+      <!-- INCLUDE HEADER.PHP start -->
+      <?php include 'sections/header.php';?>
+      <!-- INCLUDE HEADER.PHP end -->
+
+      
+
+    </div>
+    <!-- FORCE MAX-WIDTH 100% end -->
+
+    <!-- INCLUDE FOOTER.PHP start -->
+    <?php include 'sections/footer.php';?>
+    <!-- INCLUDE FOOTER.PHP end -->
+
+
+  </body>
+
+</html>
