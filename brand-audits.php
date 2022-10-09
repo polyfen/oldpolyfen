5763d34428288ca8d2c985c07a9fc7878265877d
Boris Hrnčić <borishrncic@users.noreply.github.com>
2022-10-09 18:10:03 +0200

    Brand Audits Initial Commit

--- brand-audits.php
+++ brand-audits.php
@@ -27,6 +27,7 @@
     <!-- PAGE-SPECIFIC META end -->
 
     <!-- PAGE-SPECIFIC CSS start -->
+    <link rel="stylesheet" href="app/css/forms.css" type="text/css">
     <!-- PAGE-SPECIFIC CSS end -->
 
     <!-- PAGE-SPECIFIC SCRIPTS start -->
@@ -49,7 +50,73 @@
       <?php include 'sections/header.php';?>
       <!-- INCLUDE HEADER.PHP end -->
 
+      <div class="content-container">
       
+        <h2 style="text-align:center;line-height:1.25;">We're giving away<br/>4 Brand Audits for free<br/>this October</h2>
+
+        <p style="max-width:500px;margin:0 auto auto;text-align:center;">Apply for a full custom report listing how your business can improve its brand and website.</p>
+   
+        <div id="form-container" style="max-width:400px;margin:60px auto 120px;">
+
+        <!--
+        <form style="text-align:center;">
+          <input placeholder="your email"></input>
+          <input placeholder="your company's website"></input>
+          <button class="button secondary-button">Apply</button>
+        </form>
+        -->
+
+        <!-- Begin Mailchimp Signup Form -->
+            <div id="mc_embed_signup">
+                <form action="https://thepolygroup.us4.list-manage.com/subscribe/post?u=737a99e56206ee60a05805a04&amp;id=5b81e19b25&amp;f_id=00c7d5e9f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self">
+                    <div id="mc_embed_signup_scroll">
+                    
+            <div class="mc-field-group half-column-input">
+              <label for="mce-FIRSTNAME">First Name
+            </label>
+              <input type="text" value="" name="FIRSTNAME" class="required" id="mce-FIRSTNAME" required>
+              <span id="mce-FIRSTNAME-HELPERTEXT" class="helper_text"></span>
+            </div>
+            <div class="mc-field-group half-column-input">
+              <label for="mce-LASTNAME">Last Name
+              </label>
+              <input type="text" value="" name="LASTNAME" class="required" id="mce-LASTNAME" required>
+              <span id="mce-LASTNAME-HELPERTEXT" class="helper_text"></span>
+            </div>
+            <div class="mc-field-group">
+              <label for="mce-EMAIL">Email Address
+            </label>
+              <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" required>
+              <span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
+            </div>
+            <div class="mc-field-group">
+              <label for="mce-COMPANYWEB">Company website
+            </label>
+              <input type="url" value="http://" name="COMPANYWEB" class="required url" id="mce-COMPANYWEB" required>
+              <span id="mce-COMPANYWEB-HELPERTEXT" class="helper_text"></span>
+            </div>
+            <div hidden="true"><input type="hidden" name="tags" value="7098756"></div>
+              <div id="mce-responses" class="clear foot">
+                <div class="response" id="mce-error-response" style="display:none"></div>
+                <div class="response" id="mce-success-response" style="display:none"></div>
+              </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_737a99e56206ee60a05805a04_5b81e19b25" tabindex="-1" value=""></div>
+                    <div class="optionalParent">
+                        <div class="clear foot" style="text-align:center;">
+                            <input type="submit" value="Apply now!" name="subscribe" id="mc-embedded-subscribe" class="button">
+                            <p class="brandingLogo" style="display:none;"><a href="http://eepurl.com/iaW-PX" title="Mailchimp - email marketing made easy and fun"><img src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg"></a></p>
+                        </div>
+                    </div>
+                </div>
+            </form>
+            </div>
+
+            <!--End mc_embed_signup-->
+
+      </div>
+
+
+      </div>
 
     </div>
     <!-- FORCE MAX-WIDTH 100% end -->
