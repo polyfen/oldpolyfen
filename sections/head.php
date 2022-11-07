c5ed6a940d6ad9d2eb448418b7f12b1e09c8ed74
Boris Hrnčić <borishrncic@users.noreply.github.com>
2022-11-07 13:42:16 +0100

    Mobile Optimized

--- sections/head.php
+++ sections/head.php
@@ -2,7 +2,7 @@
 
   <!-- META General -->
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
   <meta property="og:type" content="website"/>
   <meta name="author"  content="The Poly Group"/>
 
