c166f2869ccabf621cc1301f769f15e615a119b3
Boris Hrnčić <borishrncic@users.noreply.github.com>
2022-12-01 16:23:06 +0100

    Updated november to december

--- sections/hello-bar.php
+++ sections/hello-bar.php
@@ -1,4 +1,4 @@
 <div class="hello-bar">
-    <small>🎉🎉🎉 We're giving away 4 Brand Audits for free this November</small>
+    <small>🎄🎄🎄 We're giving away 4 Brand Audits for free this December</small>
     <a href="/brand-audits"><button class="button secondary-button">Apply now!</button></a>
 </div>
