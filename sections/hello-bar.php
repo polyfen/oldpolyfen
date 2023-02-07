ca31c670ea97ce32792085609abd2fef9eff0d22
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-02-07 10:15:53 +0100

    Update hello-bar.php

--- sections/hello-bar.php
+++ sections/hello-bar.php
@@ -3,5 +3,5 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/DatesUtilsPolyfen.php');
 ?>
 <div class="hello-bar">
     <small><?= DatesUtilsPolyfen::getEmojiHelloBar() ?> Schedule a free call to get branding advice for your business</small>
-    <a href="/schedule"><button class="button secondary-button">Learn more!</button></a>
+    <a href="/schedule"><button class="button secondary-button">Learn more</button></a>
 </div>
