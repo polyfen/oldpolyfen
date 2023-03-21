8f0cbdf64ecdf7e4ca52412cfae613ab0f356d4d
xxiicode <leandrobilokapic@gmail.com>
2023-03-21 20:28:50 +0100

    forget to modify the hellobar.php
    is restored now.

--- sections/hello-bar.php
+++ sections/hello-bar.php
@@ -1,5 +1,5 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'] . '/polyfen/includes/DatesUtilsPolyfen.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/DatesUtilsPolyfen.php');
 ?>
 <div class="hello-bar">
     <small><?= DatesUtilsPolyfen::getEmojiHelloBar() ?> Schedule a free call to get branding advice for your business</small>
