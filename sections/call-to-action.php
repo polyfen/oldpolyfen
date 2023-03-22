31da6c9a89f471ab083fc6e63ef21deee885aba4
xxiicode <leandrobilokapic@gmail.com>
2023-03-22 15:55:00 +0100

    added button
    After several observations, this is the best and cleaner version

--- sections/call-to-action.php
+++ sections/call-to-action.php
@@ -3,6 +3,10 @@
     <h2><small class="section-name">CONTACT</small></h2>
     <h2>Give your business the brand it deserves</h2>
     <h2><a href="mailto:hi@polyfen.com">hi@polyfen.com</a></h2>
+    <h2><small>or</small><h2>
+      <a href="#">
+        <button class="secondary-button" id="schedule-a-call">schedule a call</button>
+      </a>
   </div>
 
 </section>
