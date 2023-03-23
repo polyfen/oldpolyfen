bfd1140301e0a7e15612db903730adf9d3884bff
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-03-23 16:45:00 +0100

    Removed Dates Utils PHP

--- sections/hello-bar.php
+++ sections/hello-bar.php
@@ -1,7 +1,4 @@
-<?php
-require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/DatesUtilsPolyfen.php');
-?>
 <div class="hello-bar">
-    <small><?= DatesUtilsPolyfen::getEmojiHelloBar() ?> Schedule a free call to get branding advice for your business</small>
+    <small>✨✨✨ Schedule a free call to get branding advice for your business</small>
     <a href="/strategy-call"><button class="button secondary-button">Learn more</button></a>
 </div>
