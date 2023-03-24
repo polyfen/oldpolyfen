95ef8b3e5871427ec3dc7138c54dc738b97a6add
xxiicode <leandrobilokapic@gmail.com>
2023-03-24 13:41:28 +0100

    fixed small h2 problem

--- sections/call-to-action.php
+++ sections/call-to-action.php
@@ -1,6 +1,6 @@
 <section id="contact">
   <div id="cta-content">
-    <h2><small class="section-name">CONTACT</small></h2>
+    <small class="section-name">CONTACT</small>
     <h2>Give your business the brand it deserves</h2>
     <h2><a href="mailto:hi@polyfen.com">hi@polyfen.com</a></h2>
   </div>
