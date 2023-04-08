ce74f5deb8945175d7ceaa6891cedfc012e96d54
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-04-08 12:38:22 +0200

    Update header.php

--- sections/header.php
+++ sections/header.php
@@ -1,12 +1,9 @@
 <header class="header position-absolute">
-
-  <a href="/" id="logo-container"><img src="imgs/polyfen-logo.svg" id="logo" alt="Polyfen"></a>
-
+  <a href="/"><img src="imgs/polyfen-logo.svg" id="logo" alt="Polyfen"></a>
   <nav>        
     <a href="/">Home</a>
     <a href="pricing.php">Pricing</a>
     <a href="about.php">About</a>
     <a href="contact.php">Contact</a>
   </nav>
-
 </header>
