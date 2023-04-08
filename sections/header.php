334d6fda8b24ea5034543965270fd8bd9c9aa1fa
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-04-08 12:17:06 +0200

    Refactoring WIP

--- sections/header.php
+++ sections/header.php
@@ -1,14 +1,12 @@
 <header class="header position-absolute">
+
   <a href="/" id="logo-container"><img src="imgs/polyfen-logo.svg" id="logo" alt="Polyfen"></a>
 
-  <!-- FIXED NAVBAR start --------------->
-  <nav class="navbar">
-        
+  <nav class="navbar">        
     <a href="/">Home</a>
     <a href="pricing.php">Pricing</a>
     <a href="about.php">About</a>
-    <a href="#">Blog</a>
     <a href="contact.php">Contact</a>
   </nav>
-  <!-- FIXED NAVBAR ends --------------->
+  
 </header>
