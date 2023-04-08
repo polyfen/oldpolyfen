059f4753ae0320d1a6d0ffb4ce250ac2f37510d0
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-04-08 12:31:01 +0200

    Reviewed header css

--- sections/header.php
+++ sections/header.php
@@ -2,11 +2,11 @@
 
   <a href="/" id="logo-container"><img src="imgs/polyfen-logo.svg" id="logo" alt="Polyfen"></a>
 
-  <nav class="navbar">        
+  <nav>        
     <a href="/">Home</a>
     <a href="pricing.php">Pricing</a>
     <a href="about.php">About</a>
     <a href="contact.php">Contact</a>
   </nav>
-  
+
 </header>
