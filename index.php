e20493ba32b2ffb0891434ab360f295a99df6e21
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-04-09 14:01:39 +0200

    Removed unnecesary php files from includes

--- index.php
+++ index.php
@@ -70,79 +70,31 @@
 
           <!-- BIKE IN MEX start -->
           <div class="featured-project">
-            <div id="bike-in-mex-cover" class="project-cover d-none" onmouseover="document.getElementById('bike-in-mex-button').className='secondary-button secondary-button-hover';document.getElementById('bike-in-mex-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('bike-in-mex-button').className='secondary-button';document.getElementById('bike-in-mex-cover').className='project-cover';">
-              <a href="/bike-in-mex.php">
-                <figure>
-                  <img src="imgs/case-studies/bike-in-mex/bike-in-mex-thumbnail.webp" loading="lazy" alt="Bike in Mex flag">
-                </figure>
+            <h4 class="heading-2">Bike in Mex</h4>
+            <p>We designed an identity focused on geometric illustrations inspired by ancient Aztec and Mayan symbols set on the vibrant colors of Mexico's flag for a bicycle rental shop based in Tulum, Mexico.</p>
+            <a href="/bike-in-mex.php" onmouseover="document.getElementById('bike-in-mex-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('bike-in-mex-cover').className='project-cover';">
+              <button class="secondary-button" id="bike-in-mex-button">View project <i class="fa fa-angle-right" aria-hidden="true"></i></button>
               </a>
-              <div class="project-preview">
-                <a href="/bike-in-mex.php">
-                  <figure>
-                    <img src="imgs/case-studies/bike-in-mex/bike-in-mex-preview.gif" loading="lazy" alt="Bike in Mex preview">
-                  </figure>
-                </a>
-              </div>
-            </div>
-            <div class="project-description">
-              <h4 class="heading-2">Bike in Mex</h4>
-              <p>We designed an identity focused on geometric illustrations inspired by ancient Aztec and Mayan symbols set on the vibrant colors of Mexico's flag for a bicycle rental shop based in Tulum, Mexico.</p>
-              <a href="/bike-in-mex.php" onmouseover="document.getElementById('bike-in-mex-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('bike-in-mex-cover').className='project-cover';">
-                <button class="secondary-button" id="bike-in-mex-button">View project <i class="fa fa-angle-right" aria-hidden="true"></i></button>
-              </a>
-            </div>
           </div>
           <!-- BIKE IN MEX end -->
 
           <!-- TRADEANA start -->
           <div class="featured-project">
-            <div id="tradeana-cover" class="project-cover" onmouseover="document.getElementById('tradeana-button').className='secondary-button secondary-button-hover';document.getElementById('tradeana-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('tradeana-button').className='secondary-button';document.getElementById('tradeana-cover').className='project-cover';">
-              <a href="/tradeana.php">
-                <figure>
-                  <img src="imgs/case-studies/tradeana/tradeana-thumbnail.webp" loading="lazy" alt="Tradeana">
-                </figure>
-              </a>
-              <div class="project-preview">
-                <a href="/tradeana.php">
-                  <figure>
-                    <img src="imgs/case-studies/tradeana/tradeana-preview.gif" loading="lazy" alt="Tradeana preview">
-                  </figure>
-                </a>
-              </div>
-            </div>
-            <div class="project-description">
-              <h4 class="heading-2">Tradeana</h4>
-              <p>Tradeana's patented software, Forex Risk Engine™, is designed by traders for traders to help them efficiently audit and manage the risk of their ForEx operations.</p>
-              <a href="/tradeana.php" onmouseover="document.getElementById('tradeana-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('tradeana-cover').className='project-cover';">
-                <button class="secondary-button" id="tradeana-button">View project <i class="fa fa-angle-right" aria-hidden="true"></i></button>
-              </a>
-            </div>
+            <h4 class="heading-2">Tradeana</h4>
+            <p>Tradeana's patented software, Forex Risk Engine™, is designed by traders for traders to help them efficiently audit and manage the risk of their ForEx operations.</p>
+            <a href="/tradeana.php" onmouseover="document.getElementById('tradeana-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('tradeana-cover').className='project-cover';">
+              <button class="secondary-button" id="tradeana-button">View project <i class="fa fa-angle-right" aria-hidden="true"></i></button>
+            </a>
           </div>
           <!-- TRADEANA end -->
 
           <!-- VILLA TEREZIJA start -->
           <div class="featured-project">
-            <div id="villa-terezija-cover" class="project-cover" onmouseover="document.getElementById('villa-terezija-button').className='secondary-button secondary-button-hover';document.getElementById('villa-terezija-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('villa-terezija-button').className='secondary-button';document.getElementById('villa-terezija-cover').className='project-cover';">
-              <a href="/villa-terezija.php">
-                <figure>
-                  <img src="imgs/case-studies/villa-terezija/villa-terezija-thumbnail.webp" loading="lazy" alt="Villa Terezija">
-                </figure>
-              </a>
-              <div class="project-preview">
-                <a href="/villa-terezija.php">
-                  <figure>
-                    <img src="imgs/case-studies/villa-terezija/villa-terezija-preview.gif" loading="lazy" alt="Villa Terezija preview">
-                  </figure>
-                </a>
-              </div>
-            </div>
-            <div class="project-description">
-              <h4 class="heading-2">Villa Terezija</h4>
-              <p>A touristic accommodation surrounded by a stunning rose garden in the quiet town of Baška on Krk Island, Croatia. Minutes walk from a sandy beach on the clear blue Adriatic Sea.</p>
-              <a href="/villa-terezija.php" onmouseover="document.getElementById('villa-terezija-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('villa-terezija-cover').className='project-cover';">
-                <button class="secondary-button" id="villa-terezija-button">View project <i class="fa fa-angle-right" aria-hidden="true"></i></button>
-              </a>
-            </div>
+            <h4 class="heading-2">Villa Terezija</h4>
+            <p>A touristic accommodation surrounded by a stunning rose garden in the quiet town of Baška on Krk Island, Croatia. Minutes walk from a sandy beach on the clear blue Adriatic Sea.</p>
+            <a href="/villa-terezija.php" onmouseover="document.getElementById('villa-terezija-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('villa-terezija-cover').className='project-cover';">
+              <button class="secondary-button" id="villa-terezija-button">View project <i class="fa fa-angle-right" aria-hidden="true"></i></button>
+            </a>
           </div>
           <!-- VILLA TEREZIJA end -->
 
--- sections/header.php
+++
@@ -1,9 +0,0 @@
-<header class="header position-absolute">
-  <a href="/"><img src="imgs/polyfen-logo.svg" id="logo" alt="Polyfen"></a>
-  <nav>        
-    <a href="/">Home</a>
-    <a href="pricing.php">Pricing</a>
-    <a href="about.php">About</a>
-    <a href="contact.php">Contact</a>
-  </nav>
-</header>
