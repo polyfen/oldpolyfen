47ff2cbf3e76ea54b290e5cbc5ba0c9275701a59
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-04-09 14:13:10 +0200

    Update includes.php

--- includes.php
+++ includes.php
@@ -107,7 +107,9 @@
     }
 
     function call_to_action(){
-        echo '<section id="contact">
+        echo '<hr>
+        
+        <section id="contact">
         <div id="cta-content" class="container">
           <h2 class="heading-1 align-center">Give your business<br>the brand it deserves</h2>
           <h2 class="heading-1 align-center"><a href="mailto:hi@polyfen.com">hi@polyfen.com</a></h2>
@@ -122,25 +124,16 @@
 
     function footer(){
         echo '<footer>
-            <div id="social-icons">
             <ul>
                 <a href="http://polyfen.com/twitter" target="_blank" title="Twitter"><li><i class="fab fa-twitter"></i></li></a>
                 <a href="http://polyfen.com/instagram" target="_blank" title="Instagram"><li><i class="fab fa-instagram"></i></li></a>
                 <a href="http://polyfen.com/linkedin" target="_blank" title="Linkedin" id="linkedin-icon"><li><i class="fab fa-linkedin-in"></i></li></a>
                 <a href="http://polyfen.com/github" target="_blank" title="Github" id="linkedin-icon"><li><i class="fab fa-github"></i></li></a>
             </ul>
-            </div>
-            <div class="d-block d-sm-none" style="clear:both;"></div>
-            <div id="legal">
             <a href="privacy.php"><small>Privacy Policy</small></a>
-            </div>
-            <div class="d-block d-sm-none" style="clear:both;"></div>
-            <div id="tpg">
             <a href="https://thepolygroup.co" target="_blank">
-                <img src="imgs/the-poly-group-low.svg" id="tpg-logo-low" class="tpg-logo" onmouseover="document.getElementById(\'tpg-logo-low\').className=\'d-none\';document.getElementById(\'tpg-logo\').className=\'tpg-logo\';">
-                <img src="imgs/the-poly-group.svg" id="tpg-logo" class="d-none" onmouseout="document.getElementById(\'tpg-logo\').className=\'d-none\';document.getElementById(\'tpg-logo-low\').className=\'tpg-logo\';">
+                <img src="imgs/the-poly-group-low.svg">
             </a>
-            </div>
         </footer>';
     }
 
