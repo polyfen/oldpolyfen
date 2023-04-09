553f700b909d3f0715e441d3ee454c2b34beae54
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-04-09 14:18:44 +0200

    Updated navbar styles

--- includes.php
+++ includes.php
@@ -98,10 +98,10 @@
         echo '<header class="header position-absolute">
         <a href="/"><img src="imgs/polyfen-logo.svg" id="logo" alt="Polyfen"></a>
         <nav>        
-          <a href="/">Home</a>
-          <a href="pricing.php">Pricing</a>
-          <a href="about.php">About</a>
-          <a href="contact.php">Contact</a>
+          <a href="/" class="uppercase">Home</a>
+          <a href="pricing.php" class="uppercase">Pricing</a>
+          <a href="about.php" class="uppercase">About</a>
+          <a href="contact.php" class="uppercase">Contact</a>
         </nav>
       </header>';
     }
