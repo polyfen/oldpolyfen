dc986fee00844aa42b07110dd7fd62cae4b1df43
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-04-09 14:20:32 +0200

    Update includes.php

--- includes.php
+++ includes.php
@@ -113,7 +113,7 @@
         <div id="cta-content" class="container">
           <h2 class="heading-1 align-center">Give your business<br>the brand it deserves</h2>
           <h2 class="heading-1 align-center"><a href="mailto:hi@polyfen.com">hi@polyfen.com</a></h2>
-          <small>or</small>
+          <small class="uppercase">or<br></small>
             <a href="strategy-call.php">
               <button class="secondary-button" id="schedule-a-call">schedule a call</button>
             </a>
