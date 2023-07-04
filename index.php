16330ae12aa9f3d920e7a7b25e912c7dee832eb5
xxiicode <leandrobilokapic@gmail.com>
2023-07-04 23:38:28 +0200

    added changes

--- index.php
+++ index.php
@@ -33,7 +33,8 @@
     <!-- PAGE-SPECIFIC SCRIPTS start -->
     <!-- PAGE-SPECIFIC SCRIPTS end -->
 
-
+<script defer src="app/js/subtle.js"></script>
+<script defer src="app/js/timezone.js"></script>
   </head>
 
   <body id="home" class="dark-mode">
@@ -184,7 +185,10 @@
     <!-- INCLUDE FOOTER.PHP start -->
       <?php include 'includes/footer.php';?>
     <!-- INCLUDE FOOTER.PHP end -->
-    
+
+    <!-- scripts location call luxon-->
+    <script src="https://moment.github.io/luxon/global/luxon.min.js"></script>
+    <!-- scripts location time end-->
   </body>
 
 </html>
