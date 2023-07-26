b328d8992f11c66a866f8fda4c5443824a644c8d
xxiicode <leandrobilokapic@gmail.com>
2023-07-26 20:28:02 +0200

    update footer.php from includes.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,15 +1,41 @@
 <footer>
-    <ul>
+      <ul>
         <li><a href="http://polyfen.com/twitter" target="_blank" title="Twitter"><i class="fab fa-twitter"></i></a></li>
         <li><a href="http://polyfen.com/instagram" target="_blank" title="Instagram"><i class="fab fa-instagram"></i></a></li>
         <li><a href="http://polyfen.com/linkedin" target="_blank" title="Linkedin" id="linkedin-icon"><i class="fab fa-linkedin-in"></i></a></li>
         <li><a href="http://polyfen.com/github" target="_blank" title="Github" id="linkedin-icon"><i class="fab fa-github"></i></a></li>
-    </ul>
-    <a href="privacy.php" target="_blank"><small>Privacy Policy</small></a>
-    <div id="tpg">
-      <a href="https://thepolygroup.co" target="_blank">
-        <img src="imgs/the-poly-group-low.svg" id="tpg-logo-low" class="tpg-logo" onmouseover="document.getElementById('tpg-logo-low').className='d-none';document.getElementById('tpg-logo').className='tpg-logo';">
-        <img src="imgs/the-poly-group.svg" id="tpg-logo" class="d-none" onmouseout="document.getElementById('tpg-logo').className='d-none';document.getElementById('tpg-logo-low').className='tpg-logo';">
-      </a>
-    </div>
-</footer>
+      </ul>
+
+      <section class="times">
+        <div data-timezone="US/Central">
+          <img src="imgs/flags/us.svg" class="country-flag">
+          <small class="caption">Austin</small>
+          <output class="caption">00:00</output>
+        </div>
+
+        <div data-timezone="America/Argentina/Buenos_Aires">
+          <img src="imgs/flags/ar.svg" class="country-flag">
+          <small class="caption">Mar del Plata</small>
+          <output class="caption">00:00</output>
+        </div>
+        <div data-timezone="Europe/Belgrade">
+        <img src="imgs/flags/hr.svg" class="country-flag">
+          <small class="caption">Zagreb</small>
+          <output class="caption">00:00</output>
+        </div>
+      </section>
+
+      <div id="tpg">
+        <a href="https://thepolygroup.co?utm_source=polyfen&utm_medium=referral" target="_blank">
+          <img src="imgs/the-poly-group-low.svg" id="tpg-logo-low" class="tpg-logo" onmouseover="document.getElementById(\'tpg-logo-low\').className=\'d-none\';document.getElementById(\'tpg-logo\').className=\'tpg-logo\';">
+          <img src="imgs/the-poly-group.svg" id="tpg-logo" class="d-none" onmouseout="document.getElementById(\'tpg-logo\').className=\'d-none\';document.getElementById(\'tpg-logo-low\').className=\'tpg-logo\';">
+        </a>
+      </div>
+      
+      <div class="footer_2line">
+        <a href="privacy.php" target="_blank"><small>Privacy Policy</small></a>
+        <span>&vert;</span>
+        <a href="http://brand.polyfen.com" target="_blank"><small>Brand Guidelines</small></a>
+      </div>
+
+    </footer>
