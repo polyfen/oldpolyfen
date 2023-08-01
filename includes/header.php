8f5507154f5b394313b199cc0f599dab1c72027d
xxiicode <leandrobilokapic@gmail.com>
2023-08-01 17:40:28 +0200

    first approche. just the icon in mobile

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,13 @@
 <header>
   <a href="/"><img src="imgs/polyfen-logo.svg" id="logo" alt="Polyfen"></a>
-  <nav class="nav">        
+  <nav class="desktop nav">        
     <a href="/" class="uppercase">Home</a>
     <a href="services.php" class="uppercase">Services</a>
     <a href="about.php" class="uppercase">About</a>
     <a href="contact.php" class="uppercase">Contact</a>
+  </nav>
+
+  <nav class="hamburger nav">
+    <svg height="32px" id="Layer_1" style="enable-background:new 0 0 32 32;" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"/></svg>
   </nav>
 </header>
