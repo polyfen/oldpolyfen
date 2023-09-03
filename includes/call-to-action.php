ad2304bd6556b7c0566cc1e16d6f02d9bc28d00f
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-09-03 21:44:29 +0200

    CTA section content change

--- includes/call-to-action.php
+++ includes/call-to-action.php
@@ -1,10 +1,11 @@
 <section id="contact">
   <div id="cta-content" class="hidden container">
     <h2 class="heading-1 align-center">Give your business<br>the brand it deserves<br>
-    <a href="mailto:hi@polyfen.com">hi@polyfen.com</a></h2>
-    <small class="uppercase">or<br></small>
-      <a href="strategy-call.php">
-        <button class="secondary-button" id="schedule-a-call">schedule a call</button>
-      </a>
+    <a href="contact.php#message">
+      <button class="secondary-button" id="send-a-message">send a message</button>
+    </a>
+    <a href="contact.php#call">
+      <button class="secondary-button" id="schedule-a-call">schedule a call</button>
+    </a>
   </div>
 </section>
