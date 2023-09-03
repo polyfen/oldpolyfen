df66f582b4e6226d3c34d5bb813a71dcace9977c
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-09-03 22:26:07 +0200

    CTA section update

--- includes/call-to-action.php
+++ includes/call-to-action.php
@@ -1,6 +1,6 @@
 <section id="contact">
   <div id="cta-content" class="hidden container">
-    <h2 class="heading-1 align-center">Give your business<br>the brand it deserves</h2>
+    <h2 class="heading-1 align-center">Give your customers<br> the experience they deserve</h2>
     <div id="cta-section-buttons">
       <a href="contact.php#message">
         <button class="secondary-button" id="send-message">send a message</button>
