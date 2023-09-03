10654659b542af37b1514541ce308eec84e26075
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-09-03 21:59:41 +0200

    CTA buttons update

--- includes/call-to-action.php
+++ includes/call-to-action.php
@@ -1,11 +1,13 @@
 <section id="contact">
   <div id="cta-content" class="hidden container">
-    <h2 class="heading-1 align-center">Give your business<br>the brand it deserves<br>
-    <a href="contact.php#message">
-      <button class="secondary-button" id="send-a-message">send a message</button>
-    </a>
-    <a href="contact.php#call">
-      <button class="secondary-button" id="schedule-a-call">schedule a call</button>
-    </a>
+    <h2 class="heading-1 align-center">Give your business<br>the brand it deserves</h2>
+    <div id="cta-section-buttons">
+      <a href="contact.php#message">
+        <button class="secondary-button" id="send-message">send a message</button>
+      </a>
+      <a href="contact.php#call">
+        <button class="secondary-button" id="schedule-call">schedule a call</button>
+      </a>
+    </div>
   </div>
 </section>
