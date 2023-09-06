2a25edf9d94ea66d0d89eb698c68699b7d4cdd6b
xxiicode <leandrobilokapic@gmail.com>
2023-09-06 20:07:21 +0200

    now CTA section opalescent

--- includes/call-to-action.php
+++ includes/call-to-action.php
@@ -3,10 +3,10 @@
     <h2 class="heading-1 align-center">Give your customers<br> the experience they deserve</h2>
     <div id="cta-section-buttons">
       <a href="contact.php#message">
-        <button class="secondary-button" id="send-message">send a message</button>
+        <div class="opal opal-rainbow-soft"><div class="opal-solid"><div id="send-message" class="opal-mask">send a message</div></div></div>
       </a>
       <a href="contact.php#call">
-        <button class="secondary-button" id="schedule-call">schedule a call</button>
+        <div class="opal opal-rainbow-soft"><div class="opal-solid"><div id="schedule-call" class="opal-mask">schedule a call</div></div></div>
       </a>
     </div>
   </div>
