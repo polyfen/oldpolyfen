c84035dd19834088c0e87857cfcb95f80347eeeb
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-09-10 19:42:02 +0200

    Update strategy-call.php

--- strategy-call.php
+++ strategy-call.php
@@ -81,7 +81,7 @@
     <div id="calendly">
       <!-- Calendly inline widget begin -->
       <div class="calendly-inline-widget"
-        data-url="https://calendly.com/borishrncic/strategy-call?hide_event_type_details=1&hide_gdpr_banner=1"
+        data-url="https://calendly.com/borishr/strategy?hide_event_type_details=1&hide_gdpr_banner=1"
         style="min-width:320px;height:700px;"></div>
       <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
       <!-- Calendly inline widget end -->
