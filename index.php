284a61533123f0b5b9761e9fc44f16c101190b33
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-09-18 16:34:09 +0200

    Intro CTA button

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
             <div style="clear:both;"></div>
 
             <a class="cta-button" href="/contact.php">
-              <div class="opal opal-rainbow-soft"><div class="opal-solid"><div class="opal-mask">Let's Talk!</div></div></div>
+              <div class="opal opal-rainbow-soft"><div class="opal-solid"><div class="opal-mask" style="background:none;rgba(255, 255, 255, .85)">Let's Talk!</div></div></div>
             </a>
           </div>
           <div class="background">
