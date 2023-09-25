4c7052aa154d824592a0b6341abe869bc7c3c1d1
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-09-25 06:58:05 +0200

    Update discovery-call.php

--- discovery-call.php
+++ discovery-call.php
@@ -85,25 +85,31 @@
       <!-- Calendly inline widget end -->
     </div>
 
-    <div class="container hiddenHeader" style="margin-top:0 !important;">
+    <hr>
 
-      <div style="clear:both;"></div>
-      <hr>
+    <div class="container-1080 hiddenHeader" style="margin-top:0 !important;">
 
-      <div id="testimonials-container">
-        <div class="testimonial">
-          <p>Boris is a talented designer and art director whose amazing creative chops are teamed with strong
-            analytic and organizational skills. He guided a discovery process for a brand identity project
-            that helped us tell the story of our new program-- and the experience was enlightening from
-            start to finish.<br />&mdash; Renee H.</p>
-        </div>
-      </div>
+      <section class="container">
+          <blockquote>It is always a pleasure working with Boris. This is my third time working with him. He has helped me with logos, branding, and website design. Boris always has a strong focus on client service, he provides consistent and valuable feedback, and his work always exceed expectations. I look forward to working with Boris on more projects! <br />&mdash; Carlos Flores</blockquote>
+          <blockquote>Boris is a highly comprehensive person. When I hired him and his agency to work on a brand identity we had a one-hour call where I explain all my vision and my ideas for the design and the brand. The final result was really great and had tons of assets to use in different display formats. Thank you Boris for your hard work and for giving a beautiful brand identity to our business, we are super happy with our brand!<br />&mdash; Matias Sanchez Moises</blockquote>
+          <blockquote>Boris has excellent skills and talent when discussing management, design, and branding. He presents us with several branding drafts and ideas that we can work on in our branding size, which is incredible. We had the opportunity to choose the best design that could fit our project, and finally, we had our branding.<br />&mdash; Cesar Ramos</blockquote>
+  </section>
 
       <div id="final-cta-container" class="align-center">
         <a href="#calendly">
           <div id="final-cta" class="opal opal-rainbow-soft"><div class="opal-solid"><div  class="opal-mask">↑ Schedule a Call ↑</div></div></div>
         </a>
       </div>
+
+      <hr>
+
+      <h2 class="heading-1 align-center">Give your business<br>the glow up it needs</h2>
+      <div id="final-cta-container" class="align-center">
+        <a href="#calendly">
+          <div id="final-cta" class="opal opal-rainbow-soft"><div class="opal-solid"><div  class="opal-mask">↑ Schedule a Call ↑</div></div></div>
+        </a>
+      </div>
+
     </div>
   </div>
   <!-- FORCE MAX-WIDTH 100% end -->
