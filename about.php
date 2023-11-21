55a0b29e1147a7cf0d44d52f5d4813f379f6a795
xxiicode <leandrobilokapic@gmail.com>
2023-11-21 13:14:11 +0100

    first look new section

--- about.php
+++ about.php
@@ -1,20 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
+<head>
     <title>About | Polyfen</title>
 
 
     <!-- INCLUDE HEAD.PHP start -->
-      <?php include 'includes/head.php';?>
+    <?php include 'includes/head.php';?>
     <!-- INCLUDE HEAD.PHP end -->
 
     <!-- PAGE-SPECIFIC META start -->
-    <meta property="og:title" content="About | Polyfen"/>
-    <meta property="og:url" content="https://polyfen.com"/>
-    <meta name="keywords" content="Creative agency, Development agency, Zagreb, Croatia, Austin, US, USA, Mar del Plata, Argentina, Full-service, Digital experiences, Business solutions, Owner-managed reliability, Remote business, End-to-end customer service, Flexible contract options, R&D innovation-oriented team, AI" />
-    <meta name="description" property="og:description" content="Fully remote creative & development agency transforming businesses with exceptional digital experiences since 2018." />
-    <meta property="og:image" content="https://polyfen.com/imgs/meta/about-meta.png"/>
+    <meta property="og:title" content="About | Polyfen" />
+    <meta property="og:url" content="https://polyfen.com" />
+    <meta name="keywords"
+        content="Creative agency, Development agency, Zagreb, Croatia, Austin, US, USA, Mar del Plata, Argentina, Full-service, Digital experiences, Business solutions, Owner-managed reliability, Remote business, End-to-end customer service, Flexible contract options, R&D innovation-oriented team, AI" />
+    <meta name="description" property="og:description"
+        content="Fully remote creative & development agency transforming businesses with exceptional digital experiences since 2018." />
+    <meta property="og:image" content="https://polyfen.com/imgs/meta/about-meta.png" />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
     <meta property="fb:app_id" content="1799272553516195" />
@@ -22,7 +24,8 @@
     <meta name="twitter:site" content="Polyfen" />
     <meta name="twitter:creator" content="@PolyfenAgency">
     <meta name="twitter:title" content="About" />
-    <meta name="twitter:description" content="Fully remote creative & development agency transforming businesses with exceptional digital experiences since 2018." />
+    <meta name="twitter:description"
+        content="Fully remote creative & development agency transforming businesses with exceptional digital experiences since 2018." />
     <meta name="twitter:image" content="https://polyfen.com/imgs/meta/about-meta.png" />
     <meta name="robots" content="index, follow">
     <!-- PAGE-SPECIFIC META end -->
@@ -35,83 +38,124 @@
     <!-- PAGE-SPECIFIC SCRIPTS end -->
 
 
-  </head>
+</head>
 
-  <body id="about" class="dark-mode">
+<body id="about" class="dark-mode">
 
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P25JFWC"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P25JFWC" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
 
     <!-- INCLUDE HELLO-BAR.PHP start -->
-      <?php include 'includes/hello-bar.php';?>
+    <?php include 'includes/hello-bar.php';?>
     <!-- INCLUDE HELLO-BAR.PHP end -->
 
     <!-- INCLUDE HEADER.PHP start -->
-      <?php include 'includes/header.php';?>
+    <?php include 'includes/header.php';?>
     <!-- INCLUDE HEADER.PHP end -->
 
     <!-- ABOUT start -->
     <main id="about">
 
-      <section id="about-intro" class="container">
-        <h1 class="caption align-center hiddenHeader">About</h1>
-        <h2 class="heading-1 align-center hiddenHeader">Where business &amp; creativity come together</h2>
-        <div class="column-to-row">
-          <div class="one-half hidden order">
-            <img src="imgs/illustrations/computer-illustration.webp">
-          </div>
-          <div class="one-half hidden">
-            <p>We're a full-service creative and development agency founded in Zagreb, Croatia in 2018 with team-members working from Croatia, Spain, Argentina, and the US.</p>
-            <p>We help businesses translate their vision into digital experiences that bring in the money.</p>
-          </div>
-        </div>
-      </section>
+        <section id="about-intro" class="container-1080">
+            <h1 class="caption align-center hiddenHeader">About</h1>
+            <h2 class="heading-1 align-center hiddenHeader">Where business &amp; creativity come together</h2>
+            <div class="column-to-row">
+                <div class="one-half hidden order">
+                    <img src="imgs/illustrations/computer-illustration.webp">
+                </div>
+                <div class="one-half hidden">
+                    <p>We're a full-service creative and development agency founded in Zagreb, Croatia in 2018 with
+                        team-members working from Croatia, Spain, Argentina, and the US.</p>
+                    <p>We help businesses translate their vision into digital experiences that bring in the money.</p>
+                </div>
+            </div>
+        </section>
 
-      <hr>
+        <hr>
+        <section class="container-1232">
+            <h3 class="heading-2 align-center">Meet our board</h3>
+            <div class="board-cards">
+
+                <div class="board-card">
+                    <img src="imgs/board-team/boris.webp" alt="Picture of Founder & Art Director" class="board-card-img">
+                    <p>Boris Hrnčić</p>
+                    <p>Founder & Art Director</p>
+                    <a href="#"><small>Linkedin</small></a>
+                </div>
+                <div class="board-card">
+                    <img src="imgs/board-team/rod.webp" alt="Picture of Tech & Develpment Advisor" class="board-card-img">
+                    <p>Rodrigo Gatica</p>
+                    <p>Tech & Develpment Advisor</p>
+                    <a href="#"><small>Linkedin</small></a>
+                </div>
+                <div class="board-card">
+                    <img src="imgs/board-team/mati.webp" alt="Picture of Sales & Operator Advisor" class="board-card-img">
+                    <p>Matías Sanchez</p>
+                    <p>Sales & Operator Advisor</p>
+                    <a href="#"><small>Linkedin</small></a>
+                </div>
+                <div class="board-card">
+                    <img src="imgs/board-team/joaquin.webp" alt="Picture of Research & Strastegy Advisor" class="board-card-img">
+                    <p>Joaquin Carrascosa</p>
+                    <p>Research & Strastegy Advisor</p>
+                    <a href="#"><small>Linkedin</small></a>
+                </div>
+        </section>
+        <hr>
 
         <section id="what-sets-us-apart" class="container-1080 hidden">
 
-          <h3 class="heading-2 align-center">What sets us apart</h3>
-          <div class="container-700">
-            <div class="">
-              <div class="acc-button">
-                <img src="imgs/icons/globe.svg" height="32px" width="auto">
-                <h4 class="heading-3">Fully Remote since 2018</h4>
-              </div>
-              <div class="acc-panel">
-              <p>We know that we can't compete with the comp packages of big tech companies. But where we can compete and win, is in flexibility. That allows us to have the best talent in the world at a sustainable cost.</p>
-              </div>
+            <h3 class="heading-2 align-center">What sets us apart</h3>
+            <div class="container-700">
+                <div class="">
+                    <div class="acc-button">
+                        <img src="imgs/icons/globe.svg" height="32px" width="auto">
+                        <h4 class="heading-3">Fully Remote since 2018</h4>
+                    </div>
+                    <div class="acc-panel">
+                        <p>We know that we can't compete with the comp packages of big tech companies. But where we can
+                            compete and win, is in flexibility. That allows us to have the best talent in the world at a
+                            sustainable cost.</p>
+                    </div>
+                </div>
+                <div class="">
+                    <div class="acc-button">
+                        <img src="imgs/icons/dna.svg" height="32px" width="auto">
+                        <h4 class="heading-3">Transparency is in our DNA</h4>
+                    </div>
+                    <div class="acc-panel">
+                        <p>Our <a href="https://business.polyfen.com" target="_blank">Business Plan</a> and <a
+                                href="https://wiki.polyfen.com" target="_blank">Team Wiki</a> are just some of the
+                            internal documentation that is publically available. We also contribute to the open-source
+                            community with public code repositories in <a
+                                href="https://github.com/orgs/thepolygroup/repositories" target="_blank">Github</a>.</p>
+                    </div>
+                </div>
+                <div class="">
+                    <div class="acc-button">
+                        <img src="imgs/icons/flask.svg" height="32px" width="auto">
+                        <h4 class="heading-3">Innovation through R&amp;D</h4>
+                    </div>
+                    <div class="acc-panel">
+                        <p>As a forward-thinking company we explore the latest techologies through internal Research &
+                            Development projects. That way, our clients count with the advantage of our expert know-how
+                            to beat the competition and never be left behind in an always moving market.</p>
+                    </div>
+                </div>
+                <div class="">
+                    <div class="acc-button">
+                        <img src="imgs/icons/person.svg" height="32px" width="auto">
+                        <h4 class="heading-3">Our Pledge to Employee Growth</h4>
+                    </div>
+                    <div class="acc-panel">
+                        <p>We invest a heavy commitment of our resources into constant education and professional growth
+                            of our team and all our team-members individually towards a purposeful and fruitfull
+                            work-life balance.</p>
+                    </div>
+                </div>
             </div>
-            <div class="">
-              <div class="acc-button">
-                <img src="imgs/icons/dna.svg" height="32px" width="auto">
-                <h4 class="heading-3">Transparency is in our DNA</h4>
-              </div>
-              <div class="acc-panel">
-                <p>Our <a href="https://business.polyfen.com" target="_blank">Business Plan</a> and <a href="https://wiki.polyfen.com" target="_blank">Team Wiki</a> are just some of the internal documentation that is publically available. We also contribute to the open-source community with public code repositories in <a href="https://github.com/orgs/thepolygroup/repositories" target="_blank">Github</a>.</p>
-              </div>
-            </div>
-            <div class="">
-              <div class="acc-button">
-                <img src="imgs/icons/flask.svg" height="32px" width="auto">
-                <h4 class="heading-3">Innovation through R&amp;D</h4>
-              </div>
-              <div class="acc-panel">
-                <p>As a forward-thinking company we explore the latest techologies through internal Research & Development projects. That way, our clients count with the advantage of our expert know-how to beat the competition and never be left behind in an always moving market.</p>
-              </div>
-            </div>
-            <div class="">
-              <div class="acc-button">
-                <img src="imgs/icons/person.svg" height="32px" width="auto">
-                <h4 class="heading-3">Our Pledge to Employee Growth</h4>
-              </div>
-              <div class="acc-panel">
-                <p>We invest a heavy commitment of our resources into constant education and professional growth of our team and all our team-members individually towards a purposeful and fruitfull work-life balance.</p>
-              </div>
-            </div>
-          </div>
         </section>
 
     </main>
@@ -119,14 +163,14 @@
 
 
     <!-- INCLUDE CALL-TO-ACTION.PHP start -->
-      <?php include 'includes/call-to-action.php';?>
+    <?php include 'includes/call-to-action.php';?>
     <!-- INCLUDE CALL-TO-ACTION.PHP end -->
 
 
     <!-- INCLUDE FOOTER.PHP start -->
-      <?php include 'includes/footer.php';?>
+    <?php include 'includes/footer.php';?>
     <!-- INCLUDE FOOTER.PHP end -->
 
-  </body>
+</body>
 
 </html>
