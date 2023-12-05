914ea5826c2fa19a72bd236fee76f75e57ef8ff4
xxiicode <leandrobilokapic@gmail.com>
2023-12-05 10:34:36 +0100

    first approach

---
+++ includes/testimonial.php
@@ -0,0 +1,35 @@
+<article id="testimonials" class="hidden container-700">
+    <h3 class="heading-2 align-center" style="margin-bottom: 8px;">Hear our success stories</h3>
+    <section class="testimonial">
+
+        <img src="/imgs/activity-cads/mare-totebag.png" height="100" width="100">
+
+        <div>
+            <ul>
+                <li>Always a pleasure working with them</li>
+                <li>They are very professional and always deliver on time</li>
+                <li>Their work always exceeds expectations</li>
+            </ul>
+            <h4 class="caption">— Carlos Flores, Metas Investments</h4>
+        </div>
+    </section>
+    <section class="testimonial">
+
+        <img src="/imgs/activity-cads/mandala.webp" height="100" width="100">
+
+        <div>
+            <p>The final result was really great and had tons of assets to use in different display formats. We are super happy with our brand!</p>
+            <h4 class="caption">— Matias Sanchez Moises, Bike in Mex</h4>
+        </div>
+    </section>
+    <section class="testimonial">
+
+        <img src="/imgs/activity-cads/soma.jpeg" height="100" width="100">
+
+        <div>
+            <p>Excellent skills and talent in project management, design, and branding.</p>
+            <h4 class="caption">— Cesar Ramos, Treadana</h4>
+        </div>
+    </section>
+
+</article>
