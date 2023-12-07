a4b8d7fca55107e1c5e936f18daeac27793b7a0d
xxiicode <leandrobilokapic@gmail.com>
2023-12-07 10:13:36 +0100

    webp imagenes made

--- index.php
+++ index.php
@@ -176,17 +176,17 @@
       <!-- new Grid section start -->
 
       <section class="container-1080 grid-box">
-        <div class="grid-item hidden"><img alt="" src="imgs/activity-cads/podcasttoapp-app.png"></div>
-        <div class="grid-item hidden"><img alt="" src="imgs/activity-cads/metas-investments-business-cards.png"></div>
-        <div class="grid-item hidden"><img alt="" src="imgs/activity-cads/hemingway-distillery-whiskey-bottle.png"></div>
+        <div class="grid-item hidden"><img alt="" src="imgs/activity-cads/podcasttoapp-app.webp"></div>
+        <div class="grid-item hidden"><img alt="" src="imgs/activity-cads/metas-investments-business-cards.webp"></div>
+        <div class="grid-item hidden"><img alt="" src="imgs/activity-cads/hemingway-distillery-whiskey-bottle.webp"></div>
         <div class="grid-item hidden"><img alt="" src="imgs/activity-cads/prepkanga-stationary.webp"></div>
-        <div class="grid-item hidden"><img alt="" src="imgs/activity-cads/the-barn-packaging.png"></div>
-        <div class="grid-item hidden"><img alt="" src="imgs/activity-cads/soma-iphone-app.png"></div>
-        <div class="grid-item hidden"><img alt="" src="imgs/activity-cads/health-and-travel-stationary.png"></div>
+        <div class="grid-item hidden"><img alt="" src="imgs/activity-cads/the-barn-packaging2.webp"></div>
+        <div class="grid-item hidden"><img alt="" src="imgs/activity-cads/soma-iphone-app.webp"></div>
+        <div class="grid-item hidden"><img alt="" src="imgs/activity-cads/health-and-travel-stationary.webp"></div>
         <div class="grid-item hidden"><img alt="" src="imgs/activity-cads/domingo.webp"></div>
-        <div class="grid-item hidden"><img alt="" src="imgs/activity-cads/altirand-ios-app-icon.png"></div>  
-        <div class="grid-item hidden"><img alt="" src="imgs/activity-cads/magusvalge-menu.png"></div>
-        <div class="grid-item hidden"><img alt="" src="imgs/activity-cads/mare-totebag.png"></div>
+        <div class="grid-item hidden"><img alt="" src="imgs/activity-cads/altirand-ios-app-icon.webp"></div>  
+        <div class="grid-item hidden"><img alt="" src="imgs/activity-cads/magusvalge-menu.webp"></div>
+        <div class="grid-item hidden"><img alt="" src="imgs/activity-cads/mare-totebag.webp"></div>
         <div class="grid-item hidden"><img alt="" src="imgs/activity-cads/photographers-website.webp"></div>
       </section>
 
