29db92ed1f58002eb9a6b7e7380976752aa29b29
xxiicode <leandrobilokapic@gmail.com>
2023-12-09 14:27:05 +0100

    sabado

--- includes/testimonial.php
+++ includes/testimonial.php
@@ -4,31 +4,34 @@
 
         <img src="/imgs/activity-cads/mare-totebag.png" height="100" width="100">
 
-        <div>
-            <ul>
+        <blockquote>
+            <ul class="hanging-list">
                 <li>Always a pleasure working with them</li>
                 <li>They are very professional and always deliver on time</li>
                 <li>Their work always exceeds expectations</li>
             </ul>
-            <h4 class="caption">— Carlos Flores, Metas Investments</h4>
-        </div>
+            <br>
+            — Carlos Flores, Metas Investments
+        </blockquote>
     </section>
-    <section class="testimonial">
 
+    <section class="testimonial">
         <img src="/imgs/activity-cads/mandala.webp" height="100" width="100">
-
-        <div>
-            <p>The final result was really great and had tons of assets to use in different display formats. We are super happy with our brand!</p>
-            <h4 class="caption">— Matias Sanchez Moises, Bike in Mex</h4>
-        </div>
+            <blockquote>
+                The final result was really great and had tons of
+                assets to use in different display formats.
+                We are super happy with our brand!
+                <br><br>
+                — Matias Sanchez Moises, Bike in Mex
+            </blockquote>
     </section>
     <section class="testimonial">
-
         <img src="/imgs/activity-cads/soma.jpeg" height="100" width="100">
-
-        <div>
-            <p>Excellent skills and talent in project management, design, and branding.</p>
-            <h4 class="caption">— Cesar Ramos, Treadana</h4>
+        <blockquote>
+            Excellent skills and talent in project management, design, and branding.
+            <br><br>
+            — Cesar Ramos, Treadana
+        </blockquote>
         </div>
     </section>
 
