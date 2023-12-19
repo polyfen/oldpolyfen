03461ffda8e727167ff6fc4437e66d238d3a33e7
xxiicode <leandrobilokapic@gmail.com>
2023-12-19 15:08:29 +0100

    test

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
             <small class="caption">Don't be shy</small>
             <div style="clear:both;"></div>
 
-            <a class="cta-button" href="discovery-call.php">
+            <a class="cta-button" href="discovery-call">
               <div class="opal opal-rainbow-soft" style="border: 2px solid #7537f8;"><div class="opal-solid"><div class="opal-mask" style="background:none;font-family: 'BiotifBold', sans-serif !important;">Let's Talk!</div></div></div>
             </a>
           </div>
@@ -89,13 +89,13 @@
           <!-- BIKE IN MEX start -->
           <div class="hidden featured-project">
             <div id="bike-in-mex-cover" class="project-cover" onmouseover="document.getElementById('bike-in-mex-button').className='button-hover';document.getElementById('bike-in-mex-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('bike-in-mex-button').className='';document.getElementById('bike-in-mex-cover').className='project-cover';">
-              <a href="/bike-in-mex.php">
+              <a href="/bike-in-mex">
                 <figure>
                   <img src="imgs/case-studies/bike-in-mex/bike-in-mex-thumbnail.webp" loading="lazy" alt="Bike in Mex flag">
                 </figure>
               </a>
               <div class="project-preview">
-                <a href="/bike-in-mex.php">
+                <a href="/bike-in-mex">
                   <figure>
                     <img src="imgs/case-studies/bike-in-mex/bike-in-mex-preview.gif" loading="lazy" alt="Bike in Mex preview">
                   </figure>
@@ -105,7 +105,7 @@
             <div class="project-description">
               <h4 class="heading-2">Bike in Mex</h4>
               <p>We designed an identity focused on geometric illustrations inspired by ancient Aztec and Mayan symbols set on the vibrant colors of Mexico's flag for a bicycle rental shop based in Tulum, Mexico.</p>
-              <a href="/bike-in-mex.php" onmouseover="document.getElementById('bike-in-mex-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('bike-in-mex-cover').className='project-cover';">
+              <a href="/bike-in-mex" onmouseover="document.getElementById('bike-in-mex-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('bike-in-mex-cover').className='project-cover';">
                 <button id="bike-in-mex-button">View project</button>
               </a>
             </div>
@@ -115,13 +115,13 @@
           <!-- TRADEANA start -->
           <div class="hidden featured-project">
             <div id="tradeana-cover" class="project-cover" onmouseover="document.getElementById('tradeana-button').className='button-hover';document.getElementById('tradeana-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('tradeana-button').className='';document.getElementById('tradeana-cover').className='project-cover';">
-              <a href="/tradeana.php">
+              <a href="/tradeana">
                 <figure>
                   <img src="imgs/case-studies/tradeana/tradeana-thumbnail.webp" loading="lazy" alt="Tradeana">
                 </figure>
               </a>
               <div class="project-preview">
-                <a href="/tradeana.php">
+                <a href="/tradeana">
                   <figure>
                     <img src="imgs/case-studies/tradeana/tradeana-preview.gif" loading="lazy" alt="Tradeana preview">
                   </figure>
@@ -131,7 +131,7 @@
             <div class="project-description">
               <h4 class="heading-2">Tradeana</h4>
               <p>Tradeana's patented software, Forex Risk Engine™, is designed by traders for traders to help them efficiently audit and manage the risk of their ForEx operations.</p>
-              <a href="/tradeana.php" onmouseover="document.getElementById('tradeana-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('tradeana-cover').className='project-cover';">
+              <a href="/tradeana" onmouseover="document.getElementById('tradeana-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('tradeana-cover').className='project-cover';">
                 <button id="tradeana-button">View project</button>
               </a>
             </div>
@@ -141,13 +141,13 @@
           <!-- VILLA TEREZIJA start -->
           <div class="hidden featured-project">
             <div id="villa-terezija-cover" class="project-cover" onmouseover="document.getElementById('villa-terezija-button').className='button-hover';document.getElementById('villa-terezija-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('villa-terezija-button').className='';document.getElementById('villa-terezija-cover').className='project-cover';">
-              <a href="/villa-terezija.php">
+              <a href="/villa-terezija">
                 <figure>
                   <img src="imgs/case-studies/villa-terezija/villa-terezija-thumbnail.webp" loading="lazy" alt="Villa Terezija">
                 </figure>
               </a>
               <div class="project-preview">
-                <a href="/villa-terezija.php">
+                <a href="/villa-terezija">
                   <figure>
                    <img src="imgs/case-studies/villa-terezija/villa-terezija-preview.gif" loading="lazy" alt="Villa Terezija preview">
                   </figure>
@@ -157,7 +157,7 @@
             <div class="project-description">
               <h4 class="heading-2">Villa Terezija</h4>
               <p>A touristic accommodation surrounded by a stunning rose garden in the quiet town of Baška on Krk Island, Croatia. Minutes walk from a sandy beach on the clear blue Adriatic Sea.</p>
-              <a href="/villa-terezija.php" onmouseover="document.getElementById('villa-terezija-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('villa-terezija-cover').className='project-cover';">
+              <a href="/villa-terezija" onmouseover="document.getElementById('villa-terezija-cover').className='project-cover project-cover-hover';" onmouseout="document.getElementById('villa-terezija-cover').className='project-cover';">
                 <button id="villa-terezija-button">View project</button>
               </a>
             </div>
