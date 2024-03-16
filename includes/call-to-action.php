5a31e17d9d03ff7337abf1096ec91d2e23d413ad
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-03-17 00:51:54 +0100

    add: Calendly to CTA section

--- includes/call-to-action.php
+++ includes/call-to-action.php
@@ -1,8 +1,8 @@
 <section id="contact">
   <div id="cta-content" class="hidden container">
-    <h2 class="heading-1 align-center" style="margin-bottom: 8px;">Give your business<br>the power-up it needs</h2>
-    <a href="/discovery-call">
-      <div class="opal opal-rainbow-soft" style="border: 2px solid #7537f8;"><div class="opal-solid"><div id="schedule-call" class="opal-mask" style="background:none;">schedule a call</div></div></div>
-    </a>
+    <h2 class="heading-1 align-center" style="margin-bottom: 8px;">Schedule a call<br>and get started</h2>
+    <!-- INCLUDE CALENDLY.PHP start -->
+    <?php include 'includes/calendly.php';?>
+    <!-- INCLUDE CALENDLY.PHP end -->
   </div>
 </section>
