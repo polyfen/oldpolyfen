85ca92c491fa2c911d6a5beea491fa03299f3c6d
xxiicode <leandrobilokapic@gmail.com>
2024-03-21 15:14:43 +0100

    update polynucleus.css location

--- includes/head.php
+++ includes/head.php
@@ -14,7 +14,7 @@
 <meta name="theme-color" content="#7537f8">
 
 <!-- CSS General -->
-<link rel="stylesheet" href="https://polynucleus.com/polynucleus.css" type="text/css">
+<link rel="stylesheet" href="https://polykit.xyz/polynucleus/polynucleus.css" type="text/css">
 <link href="app/css/opalescent.css" rel="stylesheet" type="text/css">
 <link rel="stylesheet" href="app/css/styles.css" type="text/css">
 
