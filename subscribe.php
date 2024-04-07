27ad8fbea7d88de1c26eca736fa32052f30d3966
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-04-07 20:37:01 +0200

    add: subscribe page (WIP)

---
+++ subscribe.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Subscribe to our Newsletter | Polyfen</title>
+
+
+    <!-- INCLUDE HEAD.PHP start -->
+    <?php include 'includes/head.php';?>
+    <!-- INCLUDE HEAD.PHP end -->
+
+    <!-- PAGE-SPECIFIC META start -->
+    <!-- PAGE-SPECIFIC META end -->
+
+    <!-- PAGE-SPECIFIC CSS start -->
+    <!-- PAGE-SPECIFIC CSS end -->
+
+    <!-- PAGE-SPECIFIC SCRIPTS start -->
+    <!-- PAGE-SPECIFIC SCRIPTS end -->
+
+</head>
+
+<body id="discovery-call" class="dark-mode landing-page">
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P25JFWC" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <!-- FORCE MAX-WIDTH 100% start -->
+    <div class="main-wrapper">
+
+        <!-- INCLUDE HEADER.PHP start -->
+        <?php include 'includes/header.php';?>
+        <!-- INCLUDE HEADER.PHP end -->
+
+        <section>
+            <h1 class="heading-1 align-center">Subscribe to our Newsletter</h1>
+        </section>
+
+    </div>
+    </div>
+    <!-- FORCE MAX-WIDTH 100% end -->
+
+    <!-- INCLUDE FOOTER.PHP start -->
+    <?php include 'includes/footer.php';?>
+    <!-- INCLUDE FOOTER.PHP end -->
+
+</body>
+
+</html>
