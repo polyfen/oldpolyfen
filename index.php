af8aa5ee2cff7cb99b49e0a98dccce4c920836d2
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-04-11 22:37:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -144,7 +144,7 @@
             <section class="column">
                 <div class="services-card">
                     <h3 class="heading-2">Sr UX<br>Designer</h3>
-                    <h4 class="caption" style="opacity: 0.5;margin-bottom:-40px;margin-top:20px;">at around</h4>
+                    <h4 class="caption" style="opacity: 0.5;margin-bottom:-40px;margin-top:20px;">Around</h4>
                     <h4 class="heading-3">$45</h4>
                     <h4 class="caption" style="opacity: 0.5;">Net 14 Billing</h4>
                 </div>
@@ -152,7 +152,7 @@
             <section class="column">
                 <div class="services-card">
                 <h3 class="heading-2">Sr Software<br>Engineer</h3>
-                <h4 class="caption" style="opacity: 0.5;margin-bottom:-40px;margin-top:20px;">at around</h4>
+                <h4 class="caption" style="opacity: 0.5;margin-bottom:-40px;margin-top:20px;">Around</h4>
                 <h4 class="heading-3">$45</h4>
                 <h4 class="caption" style="opacity: 0.5;">Net 14 Billing</h4>
                 </div>
@@ -160,7 +160,7 @@
             <section class="column">
             <div class="services-card">
             <h3 class="heading-2">Sr Scrum<br>Master</h3>
-            <h4 class="caption" style="opacity: 0.5;margin-bottom:-40px;margin-top:20px;">at around</h4>
+            <h4 class="caption" style="opacity: 0.5;margin-bottom:-40px;margin-top:20px;">Around</h4>
             <h4 class="heading-3">$35</h4>
             <h4 class="caption" style="opacity: 0.5;">Net 14 Billing</h4>
             </div>
