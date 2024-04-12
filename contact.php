33dac8f5ad5d0972333bedd1b3eec2fd8037891d
xxiicode <leandrobilokapic@gmail.com>
2024-04-12 12:57:36 +0200

    refactoring: work-page terminada primer approach

--- contact.php
+++ contact.php
@@ -45,7 +45,7 @@
 
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P25JFWC" height="0" width="0"
-            style="display:none;visibility: "></iframe></noscript>
+            style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
 
 
