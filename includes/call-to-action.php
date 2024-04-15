a62d2e597a0906a03b106a9739e04c44171169d2
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-04-15 16:41:07 +0200

    improve: design of CTA section

--- includes/call-to-action.php
+++ includes/call-to-action.php
@@ -4,7 +4,7 @@
       <div class="hidden container">
         <h2 class="heading-1 align-center" style="margin-bottom: 8px;">Give your business<br>the power-up it needs</h2>
         <a href="/discovery-call">
-          <div class="opal opal-rainbow-soft" style="border: 2px solid #7537f8;"><div class="opal-solid"><div id="schedule-call" class="opal-mask" style="background:none;">schedule a call</div></div></div>
+          <button id="schedule-call">schedule a call</button>
         </a>
       </div>
     </div>
