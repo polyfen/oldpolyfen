1b1cf1832430b96d4e58446ac45e96e9407b2a28
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-04-16 23:10:45 +0200

    improve: text styles

--- includes/head.php
+++ includes/head.php
@@ -43,7 +43,9 @@
 */
 -->
 <link rel="stylesheet" type="text/css" href="/libs/fonts/Biotif.css">
-<link rel="stylesheet" href="https://use.typekit.net/aba8ecz.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 <script src="https://kit.fontawesome.com/d00980734d.js" crossorigin="anonymous"></script>
 <!-- GTM -->
