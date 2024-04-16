8f7dcdfc9d54f2cdb9bf7a9609a35814d12cb2fb
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-04-17 00:08:53 +0200

    improve: display and captions font-family

--- includes/head.php
+++ includes/head.php
@@ -26,26 +26,10 @@
 
 
 <!-- FONTS -->
-<!--
-/**
-* @license
-* MyFonts Webfont Build ID 568563
-*
-* The fonts listed in this notice are subject to the End User License
-* Agreement(s) entered into by the website owner. All other parties are
-* explicitly restricted from using the Licensed Webfonts(s).
-*
-* You may obtain a valid license from one of MyFonts official sites.
-* http://www.fonts.com
-* http://www.myfonts.com
-* http://www.linotype.com
-*
-*/
--->
-<link rel="stylesheet" type="text/css" href="/libs/fonts/Biotif.css">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 <script src="https://kit.fontawesome.com/d00980734d.js" crossorigin="anonymous"></script>
 <!-- GTM -->
