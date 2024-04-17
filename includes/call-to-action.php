3b4fc9756cdcb3257d9cde56ece020465e96a7a1
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-04-17 03:50:31 +0200

    improve: buttons

--- includes/call-to-action.php
+++ includes/call-to-action.php
@@ -3,7 +3,7 @@
         <div class="cta-content">
             <div class="hidden container">
                 <h2 class="heading-2 align-center">Give your business<br>the power-up it needs</h2>
-                <a class="button large-button" id="schedule-call" href="/discovery-call">Schedule a Call</a>
+                <a class="button large-button secondary-button" id="schedule-call" href="/discovery-call">Schedule a Call</a>
             </div>
         </div>
     </div>
