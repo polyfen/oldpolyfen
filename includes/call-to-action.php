5848b0f3ec3219c089c906e507a882cac57668a7
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-04-17 03:37:04 +0200

    improve: CTA section design

--- includes/call-to-action.php
+++ includes/call-to-action.php
@@ -1,4 +1,10 @@
-<section class="cta-content">
-    <h2 class="heading-1 align-center">Give your business<br>the power-up it needs</h2>
-    <a class="button large-button animated-button" id="schedule-call" href="/discovery-call">Schedule a Call</a>
+<section class="cta-section">
+    <div class="grain-texture">
+        <div class="cta-content">
+            <div class="hidden container">
+                <h2 class="heading-2 align-center">Give your business<br>the power-up it needs</h2>
+                <a class="button large-button" id="schedule-call" href="/discovery-call">Schedule a Call</a>
+            </div>
+        </div>
+    </div>
 </section>
