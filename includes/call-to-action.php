502f7a0c7bb694e32ae31d0d609020ac6833c8db
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-04-17 18:24:21 +0200

    improve: CTA section heading size

--- includes/call-to-action.php
+++ includes/call-to-action.php
@@ -2,7 +2,7 @@
     <div class="grain-texture">
         <div class="cta-content">
             <div class="hidden container">
-                <h2 class="heading-2 align-center">Give your business<br>the power-up it needs</h2>
+                <h2 class="heading-1 align-center">Give your business<br>the power-up it needs</h2>
                 <a class="button large-button secondary-button" id="schedule-call" href="/discovery-call">Schedule a Call</a>
             </div>
         </div>
