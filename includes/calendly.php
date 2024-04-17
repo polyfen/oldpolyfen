af804c8c48f4ff07d676eef7843a9b93dc79b9d9
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-04-17 02:58:46 +0200

    improve: contact page design

--- includes/calendly.php
+++ includes/calendly.php
@@ -1,5 +1,4 @@
 <!-- Calendly inline widget begin -->
-<div class="hidden calendly-inline-widget" data-url="https://calendly.com/borishr/discovery?hide_event_type_details=1&hide_gdpr_banner=1&background_color=ffffff&text_color=070924&primary_color=7537f8" style="min-width:320px;height:700px;"></div>
-   <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
-</div>
+<div class="hidden calendly-inline-widget" data-url="https://calendly.com/borishr/discovery?hide_event_type_details=1&hide_gdpr_banner=1&background_color=070924&text_color=ffffff&primary_color=722fff" style="min-width:320px;height:700px;"></div>
+<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
 <!-- Calendly inline widget end -->
