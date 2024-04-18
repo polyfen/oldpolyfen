b36eda9dfa472fb6b0b0a5d5d0e4880a5b5ddab3
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-04-18 21:59:12 +0200

    add: small button class to hellobar

--- includes/hello-bar.php
+++ includes/hello-bar.php
@@ -1,4 +1,4 @@
 <div class="hello-bar">
     <small class="mini align-center">✨✨✨ Schedule a call to enhance your solutions</small>
-    <a class="button secondary-button" href="/discovery-call">Schedule a Call</a>
+    <a class="button secondary-button small-button" href="/discovery-call">Schedule a Call</a>
 </div>
