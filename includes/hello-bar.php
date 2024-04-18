999ed99d30624c8718b5b98faa0054a0328c75f6
xxiicode <leandrobilokapic@gmail.com>
2024-04-18 13:54:59 +0200

    hotfix hellobar secundary-button class and margin-inline

--- includes/hello-bar.php
+++ includes/hello-bar.php
@@ -1,4 +1,4 @@
 <div class="hello-bar">
-    <small>✨✨✨ Schedule a call to enhance your solutions</small>
-    <a href="/discovery-call">Schedule a Call</a>
+    <small class="mini align-center">✨✨✨ Schedule a call to enhance your solutions</small>
+    <a class="button secundary-button" href="/discovery-call">Schedule a Call</a>
 </div>
