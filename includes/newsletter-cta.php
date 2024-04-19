f83a001d3bb69078875c3335726c9fab7fa26959
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-04-19 23:00:40 +0200

    content update

--- includes/newsletter-cta.php
+++ includes/newsletter-cta.php
@@ -2,7 +2,7 @@
     <div class="grain-texture">
         <div class="cta-content">
             <div class="hidden container">
-                <h2 class="heading-1 align-center">Unlock Insights and<br>Stay Ahead</h2>
+                <h2 class="heading-1 align-center">Unlock Insights &amp;<br>Stay Ahead</h2>
                 <?php include 'includes/mailchimp-form.php';?>
             </div>
         </div>
