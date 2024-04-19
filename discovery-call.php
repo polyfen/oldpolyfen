91cd6144d274ebc790c515f60d6119e41c9952d8
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-04-20 01:38:58 +0200

    Update discovery-call.php

--- discovery-call.php
+++ discovery-call.php
@@ -51,7 +51,7 @@
 
     <!-- Discovery Call - Landing page start -->
     <main class="discovery-main">
-        <h1 class="heading-1 align-center">Book a discovery call</h1>
+        <h1 class="heading-1 align-center">Book a Discovery Call</h1>
 
 
         <section class="boris-container container">
