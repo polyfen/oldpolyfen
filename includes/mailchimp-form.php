52bc55e5c113b81d75d6bf143383e3beee365025
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-04-22 14:31:54 +0200

    add: mailchimp form to /subscribe

--- includes/mailchimp-form.php
+++ includes/mailchimp-form.php
@@ -1,4 +1,24 @@
-<form style="width: fit-content;margin: auto;">
-    <input placeholder="Enter your email" style="padding:1rem;width:280px;">
+<div id="mc_embed_shell">
+  
+<div id="mc_embed_signup">
+    <form action="https://polyfen.us18.list-manage.com/subscribe/post?u=d962a058bcdc55253835fc75d&amp;id=e6c751b26b&amp;f_id=007c7fe1f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self" novalidate="" style="width: fit-content; margin: 0 auto;">
+        <div id="mc_embed_signup_scroll">
+            <div class="mc-field-group"><input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" value="" placeholder="Enter your email" style="width: 240px;"><span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span></div>
+<div hidden=""><input type="hidden" name="tags" value="2189752"></div>
+        <div id="mce-responses" class="clear foot">
+            <div class="response" id="mce-error-response" style="display: none;"></div>
+            <div class="response" id="mce-success-response" style="display: none;"></div>
+        </div>
+    <div aria-hidden="true" style="position: absolute; left: -5000px;">
+        /* real people should not fill this in and expect good things - do not remove this or risk form bot signups */
+        <input type="text" name="b_d962a058bcdc55253835fc75d_e6c751b26b" tabindex="-1" value="">
+    </div>
+        <div class="optionalParent">
+            <div class="clear foot">
+                <input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button secondary-button" value="📫 Subscribe Now!">
+            </div>
+        </div>
+    </div>
 </form>
-<a class="button secondary-button" style="width: 220px;">📫 Subscribe Now!</a>
+</div>
+</div>
