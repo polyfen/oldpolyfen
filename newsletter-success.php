ff73a5b6bbab07af296ee9e85f694bf084b53237
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-04-22 16:10:38 +0200

    Create newsletter-success.php

---
+++ newsletter-success.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Subscription Confirmed | Polyfen</title>
+
+
+    <!-- INCLUDE HEAD.PHP start -->
+    <?php include 'includes/head.php';?>
+    <!-- INCLUDE HEAD.PHP end -->
+
+    <!-- PAGE-SPECIFIC META start -->
+    <meta property="og:title" content="Subscription Confirmed | Polyfen" />
+    <meta property="og:url" content="https://polyfen.com" />
+    <meta name="keywords"
+        content="" />
+    <meta name="description" property="og:description" content="" />
+    <meta property="og:image" content="https://polyfen.com/imgs/meta/polyfen-meta.png" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta property="fb:app_id" content="1799272553516195" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="Polyfen" />
+    <meta name="twitter:creator" content="@PolyfenAgency">
+    <meta name="twitter:title" content="Contact" />
+    <meta name="twitter:description" content="" />
+    <meta name="twitter:image" content="https://polyfen.com/imgs/meta/polyfen-meta.png" />
+    <meta name="robots" content="noindex, nofollow">
+    <!-- PAGE-SPECIFIC META end -->
+
+    <!-- PAGE-SPECIFIC CSS start -->
+
+    <!-- PAGE-SPECIFIC CSS end -->
+
+    <!-- PAGE-SPECIFIC SCRIPTS start -->
+    <!-- PAGE-SPECIFIC SCRIPTS end -->
+</head>
+
+<body class="thank-you dark-mode">
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P25JFWC" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+
+    <!-- INCLUDE HELLO-BAR.PHP start -->
+    <?php include 'includes/hello-bar.php';?>
+    <!-- INCLUDE HELLO-BAR.PHP end -->
+
+    <!-- INCLUDE HEADER.PHP start -->
+    <?php include 'includes/header.php';?>
+    <!-- INCLUDE HEADER.PHP end -->
+
+    <!-- THANK-YOU page start -->
+    <main class="thank-you-main">
+        <h1 class="heading-2 align-center">Subscription Confirmed!</h1>
+
+
+        <?php include 'includes/call-to-action.php';?>
+
+    </main>
+
+
+    <!-- INCLUDE FOOTER.PHP start -->
+    <?php include 'includes/footer.php';?>
+    <!-- INCLUDE FOOTER.PHP end -->
+
+</body>
+
+</html>
